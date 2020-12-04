e04c14fbd1799756127e3e07e1c3ddd853b220fd
Jan Amann <jan@amann.me>
2020-12-04 08:38:41 +0100

    Use extracted lib

--- packages/next-intl/src/index.tsx
+++ packages/next-intl/src/index.tsx
@@ -1,5 +1,2 @@
 export {default as NextIntlProvider} from './NextIntlProvider';
-export {default as NextIntlMessages} from './NextIntlMessages';
-export {default as useTranslations} from './useTranslations';
-export {default as TranslationValues} from './TranslationValues';
-export {default as useIntl} from './useIntl';
+export * from 'use-intl';
