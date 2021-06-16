fc5987156c2a74a9703e39f2b7ee19b84165fd77
Jan Amann <jan@amann.me>
2021-06-16 10:22:31 +0200

    feat: Support Next.js 11 (#36)
    * feat: Upgrade to latest Next.js version
    
    * Disable ESLint integration
    
    * Proper ESLint integration

--- packages/example/.eslintrc.js
+++ packages/example/.eslintrc.js
@@ -1,7 +1,7 @@
 require('eslint-config-molindo/setupPlugins');
 
 module.exports = {
-  extends: ['molindo/typescript', 'molindo/react'],
+  extends: ['molindo/typescript', 'molindo/react', 'plugin:@next/next/recommended'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
