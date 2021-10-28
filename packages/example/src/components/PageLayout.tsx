40d1f3b88b13afc5e4fc973a70217a9c7d5c6dc9
Jan Amann <jan@amann.me>
2021-10-28 12:54:30 +0200

    docs: Improve guide for splitting of messages

--- packages/example/src/components/PageLayout.tsx
+++ packages/example/src/components/PageLayout.tsx
@@ -1,25 +1,36 @@
+import {useTranslations} from 'next-intl';
+import Head from 'next/head';
 import {ReactNode} from 'react';
 import Navigation from './Navigation';
 
 type Props = {
   children: ReactNode;
-  title: ReactNode;
+  title: string;
 };
 
 export default function PageLayout({children, title}: Props) {
+  const t = useTranslations('PageLayout');
+
   return (
-    <div
-      style={{
-        padding: 24,
-        fontFamily: 'system-ui, sans-serif',
-        lineHeight: 1.5
-      }}
-    >
-      <Navigation />
-      <div style={{maxWidth: 510}}>
-        <h1>{title}</h1>
-        {children}
+    <>
+      <Head>
+        <title>{[title, t('pageTitle')].join(' - ')}</title>
+      </Head>
+      <div
+        style={{
+          padding: 24,
+          fontFamily: 'system-ui, sans-serif',
+          lineHeight: 1.5
+        }}
+      >
+        <Navigation />
+        <div style={{maxWidth: 510}}>
+          <h1>{title}</h1>
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
+
+PageLayout.messages = ['PageLayout', ...Navigation.messages];
--- packages/example/src/pages/index.tsx
+++ packages/example/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import pick from 'lodash/pick';
 import {GetStaticPropsContext} from 'next';
 import {useTranslations} from 'next-intl';
 import {useRouter} from 'next/router';
@@ -20,13 +21,20 @@ export default function Index() {
   );
 }
 
-export function getStaticProps({locale}: GetStaticPropsContext) {
+// The namespaces can be generated based on used components. `PageLayout` in
+// turn requires messages for `Navigation` and therefore a recursive list of
+// namespaces is created dynamically, where the owner of a component doesn't
+// have to know which nested components are rendered. Note that this approach
+// is limited to components which are not lazy loaded.
+Index.messages = ['Index', ...PageLayout.messages];
+
+export async function getStaticProps({locale}: GetStaticPropsContext) {
   return {
     props: {
-      messages: {
-        ...require(`../../messages/shared/${locale}.json`),
-        ...require(`../../messages/index/${locale}.json`)
-      }
+      messages: pick(
+        await import(`../../messages/${locale}.json`),
+        Index.messages
+      )
     }
   };
 }
