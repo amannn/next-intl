6ceee03b02128d709501bd8567c47a8a328bc19a
Jan Amann <jan@amann.me>
2021-11-24 09:39:38 +0100

    feat: Add `useLocale` and `useTimeZone`

--- packages/use-intl/src/index.tsx
+++ packages/use-intl/src/index.tsx
@@ -6,7 +6,9 @@ export {
   RichTranslationValues
 } from './TranslationValues';
 export {default as useIntl} from './useIntl';
+export {default as useLocale} from './useLocale';
 export {default as useNow} from './useNow';
+export {default as useTimeZone} from './useTimeZone';
 export {default as Formats} from './Formats';
 export {default as DateTimeFormatOptions} from './DateTimeFormatOptions';
 export {default as NumberFormatOptions} from './NumberFormatOptions';
---
+++ packages/use-intl/test/useTimeZone.test.tsx
@@ -0,0 +1,31 @@
+import {render, screen} from '@testing-library/react';
+import React from 'react';
+import {IntlProvider, useTimeZone} from '../src';
+
+it('returns the time zone when it is configured', () => {
+  function Component() {
+    return <>{useTimeZone()}</>;
+  }
+
+  render(
+    <IntlProvider locale="de" timeZone="Europe/Berlin">
+      <Component />
+    </IntlProvider>
+  );
+
+  screen.getByText('Europe/Berlin');
+});
+
+it('returns undefined when no time zone is configured', () => {
+  function Component() {
+    return <>{useTimeZone()}</>;
+  }
+
+  const {container} = render(
+    <IntlProvider locale="de">
+      <Component />
+    </IntlProvider>
+  );
+
+  expect(container.textContent).toBe('');
+});
