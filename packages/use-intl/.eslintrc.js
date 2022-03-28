4bbb411b5390c811847dd030b4ae36f05d588fb9
Jan Amann <jan@amann.me>
2022-03-28 14:48:15 +0200

    More cleanup

--- packages/use-intl/.eslintrc.js
+++ packages/use-intl/.eslintrc.js
@@ -2,12 +2,13 @@ require('eslint-config-molindo/setupPlugins');
 
 module.exports = {
   extends: ['molindo/typescript', 'molindo/react'],
-  rules: {
-    '@typescript-eslint/no-empty-interface': 'off'
-  },
 
   // The TypeScript parser for ESLint is currently unable to parse these files
-  ignorePatterns: ['NestedKeyOf.tsx', 'NestedValueOf.tsx', 'useTranslations.tsx'],
+  ignorePatterns: [
+    'NestedKeyOf.tsx',
+    'NestedValueOf.tsx',
+    'useTranslations.tsx'
+  ],
   overrides: [
     {
       files: ['./src/useTranslationsImpl.tsx', './src/utils/**/*.tsx'],
