9dcdb6b7aaebcccfc47cfdb5b0ce18916e178869
Jan Amann <jan@amann.me>
2022-04-28 22:53:27 +0200

    Remove unnecessary prop

--- packages/use-intl/test/useTranslations.test.tsx
+++ packages/use-intl/test/useTranslations.test.tsx
@@ -546,14 +546,14 @@ describe('error handling', () => {
     screen.getByText('rich');
   });
 
-  it('allows null values', () => {
+  it('allows null values for messages', () => {
     const onError = jest.fn();
 
     render(
       <IntlProvider
-        getMessageFallback={() => 'fallback'}
         locale="en"
-        // @ts-expect-error
+        // @ts-expect-error The types don't allow this,
+        // but this shouldn't lead to an error.
         messages={{a: null}}
         onError={onError}
       >
