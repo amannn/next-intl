07729501c660e3fbe31319534c68ec83a6e80f84
Jan Amann <jan@amann.me>
2022-04-28 16:08:37 +0200

    ESM build for browser

---
+++ packages/use-intl/dts.config.js
@@ -0,0 +1,13 @@
+/* global module */
+
+/**
+ * @type {import('dts-cli').DtsConfig}
+ */
+module.exports = {
+  rollup(config) {
+    if (config.output.format === 'esm') {
+      config.output.preserveModules = true;
+    }
+    return config;
+  }
+};
