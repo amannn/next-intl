a3c6eb377cd308a876159684d3cc77bd7bb6798f
Jan Amann <jan@amann.me>
2022-12-08 18:07:38 +0100

    Fix TS

--- packages/example-next-13-advanced/src/app/[locale]/page.tsx
+++ packages/example-next-13-advanced/src/app/[locale]/page.tsx
@@ -14,6 +14,7 @@ export default function Index() {
       <CurrentTime />
       <LocaleSwitcher />
       <MessagesAsPropsCounter />
+      {/* @ts-expect-error Server Component */}
       <MessagesOnClientCounter />
     </PageLayout>
   );
