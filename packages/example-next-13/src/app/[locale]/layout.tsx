befbf03a3a3b65bec68d2eacbdd80f5f5b122521
Jan Amann <jan@amann.me>
2022-12-20 16:00:06 +0100

    Migrate basic example

--- packages/example-next-13/src/app/[locale]/layout.tsx
+++ packages/example-next-13/src/app/[locale]/layout.tsx
@@ -1,5 +1,4 @@
 import {NextIntlServerProvider} from 'next-intl/server';
-import {notFound} from 'next/navigation';
 import {ReactNode} from 'react';
 
 type Props = {
@@ -11,16 +10,7 @@ export default async function LocaleLayout({
   children,
   params: {locale}
 }: Props) {
-  let messages;
-  try {
-    messages = (await import(`../../../messages/${locale}.json`)).default;
-  } catch (error) {
-    notFound();
-  }
-
   return (
-    <NextIntlServerProvider locale={locale} messages={messages}>
-      {children}
-    </NextIntlServerProvider>
+    <NextIntlServerProvider locale={locale}>{children}</NextIntlServerProvider>
   );
 }
--- packages/next-intl/src/server/staticConfig.tsx
+++ packages/next-intl/src/server/staticConfig.tsx
@@ -1,4 +1,5 @@
 // @ts-expect-error Should we provide a default? Probably.
+// eslint-disable-next-line import/no-extraneous-dependencies
 import staticConfig from 'next-intl/config';
 import NextIntlConfig from './NextIntlConfig';
 
