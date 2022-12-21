4f11233b6348f1ddcd9da29b6e972103849f4791
Jan Amann <jan@amann.me>
2022-12-21 23:17:12 +0100

    Use `next.config.js` instead of `tsconfig.json` for injecting config

--- packages/example-next-13-advanced/.eslintrc.js
+++ packages/example-next-13-advanced/.eslintrc.js
@@ -1,10 +1,15 @@
 require('eslint-config-molindo/setupPlugins');
 
 module.exports = {
-  extends: ['molindo/typescript', 'molindo/react', 'plugin:@next/next/recommended'],
+  env: {
+    node: true
+  },
+  extends: [
+    'molindo/typescript',
+    'molindo/react',
+    'plugin:@next/next/recommended'
+  ],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/anchor-is-valid': 'off',
-    'react/display-name': 'off'
+    'react/react-in-jsx-scope': 'off'
   }
-}
+};
