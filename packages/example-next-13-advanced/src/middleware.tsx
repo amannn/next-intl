4f91db5d86820142df1f97a73ef403a036aeb086
Jan Amann <jan@amann.work>
2023-01-09 14:44:02 +0100

    Showcase static conf

--- packages/example-next-13-advanced/src/middleware.tsx
+++ packages/example-next-13-advanced/src/middleware.tsx
@@ -1,6 +1,14 @@
 import {createIntlMiddleware} from 'next-intl/server';
 
-export default createIntlMiddleware();
+export default createIntlMiddleware({
+  // By default the `timeZone` of the server
+  // is used, but this can be overridden.
+  timeZone: 'America/New_York',
+
+  // By default the current time of the server is used,
+  // but you can override this (e.g. for test assertions).
+  now: new Date(2020, 0, 1, 12, 0, 0)
+});
 
 export const config = {
   // Skip all internal paths
