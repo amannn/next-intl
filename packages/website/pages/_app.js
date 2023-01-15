544a24fb98425b51f3a31aa6bd41d50751743b34
April <95392008+apriltaoyvr@users.noreply.github.com>
2023-01-15 15:51:32 +0100

    Remove styles.css

--- packages/website/pages/_app.js
+++ packages/website/pages/_app.js
@@ -1,5 +1,4 @@
 import 'nextra-theme-docs/style.css';
-import '../styles/styles.css';
 
 export default function App({Component, pageProps}) {
   const getLayout = Component.getLayout || ((page) => page);
