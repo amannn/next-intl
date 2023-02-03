c5ea64d744172fc0ade5e4691b336b2179cbc62e
Jan Amann <jan@amann.work>
2023-02-03 09:44:42 +0100

    Improve error handling for async components

--- packages/next-intl/src/react-server/useConfig.tsx
+++ packages/next-intl/src/react-server/useConfig.tsx
@@ -28,11 +28,43 @@ export default function useConfig() {
     return isPromise(valueOrPromise) ? use(valueOrPromise) : valueOrPromise;
   }
 
-  return useMemo(() => {
-    const runtimeConfig = safeUnwrap(
-      receiveRuntimeConfig(locale, createRequestConfig)
-    );
-    const opts = {...runtimeConfig, locale};
-    return getIntlContextValue(opts);
-  }, [locale]);
+  try {
+    return useMemo(() => {
+      const runtimeConfig = safeUnwrap(
+        receiveRuntimeConfig(locale, createRequestConfig)
+      );
+      const opts = {...runtimeConfig, locale};
+      return getIntlContextValue(opts);
+    }, [locale]);
+  } catch (error) {
+    if (
+      error instanceof TypeError &&
+      error.message.includes("reading 'useMemo'")
+    ) {
+      throw new Error(
+        `\`next-intl\` is currently not supported in async components. Please turn your component into a regular component by removing the \`async\` keyword and replacing \`await\` with \`use\` in the component body.
+
+Before:
+
+async function Component() {
+  const response = await fetch(...);
+  // ...
+}
+
+After:
+
+function Component() {
+  const response = use(fetch(...)));
+  // ...
+}
+
+Alternatively, you can split your component to separate the async code from the usage of \`next-intl\`.
+
+See https://github.com/vercel/next.js/issues/44778
+`
+      );
+    } else {
+      throw error;
+    }
+  }
 }
