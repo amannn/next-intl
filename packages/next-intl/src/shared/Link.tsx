7a76d2bfda0285cc02ff9bc8ac1414711e3102c4
Jan Amann <jan@amann.me>
2023-02-19 07:17:52 +0100

    Default to disabling prefetching in `Link` when a `locale` is passed (closes #193)

--- packages/next-intl/src/shared/Link.tsx
+++ packages/next-intl/src/shared/Link.tsx
@@ -12,15 +12,25 @@ type Props = Omit<ComponentProps<typeof NextLink>, 'locale'> & {
 /**
  * Wraps `next/link` and prefixes the `href` with the current locale.
  */
-function Link({href, locale, ...rest}: Props, ref: Props['ref']) {
+function Link({href, locale, prefetch, ...rest}: Props, ref: Props['ref']) {
   const [localizedHref, setLocalizedHref] = useState<typeof href>(href);
   const pathname = usePathname();
+
+  // When the user renders a link to another locale, Next.js might prefetch it
+  // and the `set-cookie` response header will cause the locale cookie on the
+  // current page to be overwritten. Therefore default to disabling prefetching
+  // when the user passes a `locale` prop.
+  if (prefetch === undefined && locale !== undefined) {
+    prefetch = false;
+  }
 
   useEffect(() => {
     setLocalizedHref(localizeHref(href, locale));
   }, [href, locale, pathname]);
 
-  return <NextLink ref={ref} href={localizedHref} {...rest} />;
+  return (
+    <NextLink ref={ref} href={localizedHref} prefetch={prefetch} {...rest} />
+  );
 }
 
 export default forwardRef(Link);
