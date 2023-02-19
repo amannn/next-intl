ee342f7a77bb0e28954eb4bb29371f9427e91345
Jan Amann <jan@amann.me>
2023-02-19 18:57:20 +0100

    Cleanup

--- packages/website/pages/_app.js
+++ packages/website/pages/_app.js
@@ -1,4 +1,5 @@
 import 'nextra-theme-docs/style.css';
+import '../styles.css';
 
 export default function App({Component, pageProps}) {
   const getLayout = Component.getLayout || ((page) => page);
--- packages/website/tailwind.config.js
+++ packages/website/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './components/**/*.js',
     './pages/**/*.{md,mdx}',
