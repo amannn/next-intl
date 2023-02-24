badca2d9ac5fdef678d011c8294b6665d107dd5e
Jan Amann <jan@amann.me>
2023-02-24 10:44:28 +0100

    Add types

---
+++ packages/next-intl/next-env.d.ts
@@ -0,0 +1,6 @@
+// Normally Next.js creates this file when the build runs. Seems like from
+// Next.js 13.2 on this is mandatory to be able to import `usePathname`.
+
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/// <reference types="next/navigation-types/navigation" />
