c65eedeea3163d9e092550b0c13f3e632cf307f5
Jan Amann <jan@amann.me>
2023-02-27 15:27:03 +0100

    De-opt locale switching with `Link`

--- packages/next-intl/src/client/localizeHref.tsx
+++ packages/next-intl/src/client/localizeHref.tsx
@@ -3,19 +3,28 @@ import localizePathname from '../shared/localizePathname';
 import getCookieLocale from './getCookieLocale';
 import hasPathnamePrefixed from './hasPathnamePrefixed';
 
-export default function localizeHref(href: string, locale?: string): string;
+export default function localizeHref(
+  href: string,
+  locale?: string,
+  pathname?: string
+): string;
 export default function localizeHref(
   href: UrlObject | string,
-  locale?: string
+  locale?: string,
+  pathname?: string
 ): UrlObject | string;
 export default function localizeHref(
   href: UrlObject | string,
-  locale?: string
+  locale?: string,
+  pathname?: string
 ) {
   const cookieLocale = getCookieLocale();
   if (!locale) locale = cookieLocale;
 
-  const {pathname} = window.location;
+  if (!pathname) {
+    pathname = window.location.pathname;
+  }
+
   const isSwitchingLocale = locale !== cookieLocale;
   const isPathnamePrefixed = hasPathnamePrefixed(locale, pathname);
 
--- packages/next-intl/src/shared/Link.tsx
+++ packages/next-intl/src/shared/Link.tsx
@@ -11,22 +11,47 @@ type Props = Omit<ComponentProps<typeof NextLink>, 'locale'> & {
 
 /**
  * Wraps `next/link` and prefixes the `href` with the current locale.
+ *
+ * Note that when a `locale` prop is passed, two de-optimizations are made:
+ *
+ * 1. The `prefetch` prop is not supported. This is because Next.js prefetches
+ *    the page and the `set-cookie` response header will cause the locale cookie
+ *    on the current page to be overwritten.
+ * 2. A regular anchor tag is used instead of `next/link`. This is to avoid a
+ *    bug with Server Components where the markup wouldn't be updated correctly
+ *    otherwise.
  */
 function Link({href, locale, prefetch, ...rest}: Props, ref: Props['ref']) {
   const [localizedHref, setLocalizedHref] = useState<typeof href>(href);
   const pathname = usePathname();
 
-  // When the user renders a link to another locale, Next.js might prefetch it
-  // and the `set-cookie` response header will cause the locale cookie on the
-  // current page to be overwritten. Therefore default to disabling prefetching
-  // when the user passes a `locale` prop.
-  if (prefetch === undefined && locale !== undefined) {
-    prefetch = false;
-  }
-
   useEffect(() => {
-    setLocalizedHref(localizeHref(href, locale));
+    setLocalizedHref(localizeHref(href, locale, pathname));
   }, [href, locale, pathname]);
+
+  if (locale !== undefined) {
+    // If Next.js fixes the bug where the markup isn't updated correctly when
+    // the locale changes, we can remove this check. Note however that we still
+    // need to disable prefetching (see comment above).
+
+    if (prefetch && process.env.NODE_ENV !== 'production') {
+      console.error(
+        'The `prefetch` prop is currently not supported when using the `locale` prop on `Link`.`'
+      );
+    }
+
+    let localizedHrefString;
+    if (typeof localizedHref === 'string') {
+      localizedHrefString = localizedHref;
+    } else if (localizedHref) {
+      localizedHrefString = localizedHref.toString();
+    }
+
+    return (
+      // eslint-disable-next-line jsx-a11y/anchor-has-content
+      <a ref={ref} href={localizedHrefString} {...rest} />
+    );
+  }
 
   return (
     <NextLink ref={ref} href={localizedHref} prefetch={prefetch} {...rest} />
