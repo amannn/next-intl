42a9b79536874b464f2262c50512f3d8ce160b0d
Jan Amann <jan@amann.me>
2023-03-06 12:05:54 +0100

    Include deprecation warning for `useIntl` in RSC

--- packages/next-intl/src/react-server/useIntl.tsx
+++ packages/next-intl/src/react-server/useIntl.tsx
@@ -1,6 +1,15 @@
 import getIntl from '../server/getIntl';
 import useHook from './useHook';
 
+let hasWarned = false;
+
 export default function useIntl() {
+  if (!hasWarned) {
+    hasWarned = true;
+    console.warn(
+      '`useIntl()` is deprecated and will be removed in the next major version. Please switch to `useFormatter()`.'
+    );
+  }
+
   return useHook('useIntl', getIntl());
 }
