8a30f81d778adcb050a047aba863d3234a6cab10
Jan Amann <jan@amann.me>
2023-03-17 09:56:13 +0100

    Link to article on Smashing Magazine

--- packages/example-next-13-named-routes/next.config.js
+++ packages/example-next-13-named-routes/next.config.js
@@ -5,17 +5,24 @@ module.exports = withNextIntl({
   experimental: {appDir: true},
 
   rewrites() {
-    // Routes are set up for the default locale (en) in the app. This
-    // rewrites all non-en routes to their internal equivalent.
+    // Routes are set up for the default locale ("en") in the app. This
+    // rewrites all non-English routes to their internal equivalent.
+    // E.g. `/gitarren/gibson-les-paul` → `/guitars/gibson-les-paul`
     const locales = Object.keys(routesByLocale).filter(
       (locale) => locale !== 'en'
     );
 
     return locales.flatMap((locale) =>
-      Object.keys(routesByLocale[locale]).map((routeName) => ({
-        source: `/${locale}${routesByLocale[locale][routeName]}`,
-        destination: `/${locale}${routesByLocale.en[routeName]}`
-      }))
+      Object.entries(routesByLocale[locale])
+
+        // No need to rewrite the root route
+        .filter(([, routePath]) => routePath !== '/')
+
+        // Rewrite the localized pathname to the equivalent route from "en"
+        .map(([routeName, routePath]) => ({
+          source: `/${locale}${routePath}`,
+          destination: `/${locale}${routesByLocale.en[routeName]}`
+        }))
     );
   }
 });
