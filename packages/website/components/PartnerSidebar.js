3490af9aa3ed0f2674c316472d2a64a8b2449997
Jan Amann <jan@amann.me>
2023-03-23 16:39:34 +0100

    Smaller label in sidebar

--- packages/website/components/PartnerSidebar.js
+++ packages/website/components/PartnerSidebar.js
@@ -3,7 +3,7 @@ import Partner from './Partner';
 export default function PartnerSidebar({intro = 'Presented by'}) {
   return (
     <div className="pb-1">
-      <p className="mb-2 text-xs uppercase tracking-wider text-slate-500 dark:text-slate-300">
+      <p className="mb-2 text-[11px] uppercase tracking-wider text-slate-500 dark:text-slate-300">
         {intro}
       </p>
       {/* eslint-disable-next-line react/jsx-no-target-blank */}
