e3f3c22326d058105e3baf420be248b16e7ad8f6
Jan Amann <jan@amann.me>
2023-03-24 13:10:40 +0100

    More visibility for partner in header

--- packages/website/components/Hero.js
+++ packages/website/components/Hero.js
@@ -13,7 +13,7 @@ export default function Hero({
 }) {
   return (
     <div className="dark overflow-hidden">
-      <div className="relative max-w-full overflow-hidden bg-slate-850 py-16 sm:px-2 lg:py-52 lg:px-0">
+      <div className="relative max-w-full overflow-hidden bg-slate-850 py-16 sm:px-2 lg:py-40 lg:px-0">
         <div className="absolute top-0 left-0 h-[20500px] w-[20500px] -translate-x-[47.5%] rounded-full bg-gradient-to-b from-slate-900 via-cyan-500 md:top-1" />
         <Wrapper>
           <div className="flex flex-col gap-16 xl:flex-row xl:items-center xl:justify-between">
