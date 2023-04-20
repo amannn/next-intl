2434ca760ee12e5a2003fb42a5767549b6b6cd4e
Jan Amann <jan@amann.me>
2023-04-20 09:03:23 +0200

    Fix tracking

--- packages/website/services/BrowserTracker.tsx
+++ packages/website/services/BrowserTracker.tsx
@@ -1,20 +1,26 @@
 import * as vercel from '@vercel/analytics';
 
+// Note: Vercel Analytics only supports 2 properties per event
+type Event = {
+  name: 'partner-referral';
+  data: {href: string; name: string};
+};
+
 export default class BrowserTracker {
-  public static trackEvent({data, name}: {data?: any; name: string}) {
+  public static trackEvent({data, name}: Event) {
+    const promises = [];
+
     if (typeof window !== 'undefined') {
       const umami = (window as any).umami;
       if (umami) {
-        return umami.trackEvent(name, data);
+        promises.push(umami.trackEvent(name, data));
       } else {
         console.warn('umami not loaded');
       }
     }
 
-    if (data && Object.keys(data).length > 2) {
-      console.error('Vercel Analytics only supports 2 properties per event');
-    }
+    promises.push(vercel.track(name, data));
 
-    vercel.track(name, data);
+    return Promise.all(promises);
   }
 }
