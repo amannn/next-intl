1fae25304b6e65422ed5c4be99e5e2ff5f935503
Jan Amann <jan@amann.work>
2023-05-04 15:25:04 +0200

    Attempt to fix turbo caching

--- scripts/prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$CI" ]; then
-  # Something is wrong with restoring cached outputs in CI.
-  # Maybe related: https://github.com/vercel/turbo/issues/4137
-  turbo run build --filter './packages/**' --force
-else
-  turbo run build --filter './packages/**'
-fi
