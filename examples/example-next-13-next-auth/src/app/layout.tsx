e3e6365c691e1234a41b06c7a12d6efe8827c974
Jan Amann <jan@amann.work>
2023-05-04 19:19:21 +0200

    Use version for packages and fix type in auth example

--- examples/example-next-13-next-auth/src/app/layout.tsx
+++ examples/example-next-13-next-auth/src/app/layout.tsx
@@ -7,6 +7,6 @@ type Props = {
 // Even though this component is just passing its children through, the presence
 // of this file fixes an issue in Next.js 13.3.0 where link clicks that switch
 // the locale would otherwise be ignored.
-export default async function RootLayout({children}: Props) {
+export default function RootLayout({children}: Props) {
   return children;
 }
