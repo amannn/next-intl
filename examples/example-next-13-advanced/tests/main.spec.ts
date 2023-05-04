782d103d7c7afc9ab0b2ce2b47ed98cfb4d71e4d
Jan Amann <jan@amann.work>
2023-05-04 20:25:11 +0200

    Update Next.js for newer examples

--- examples/example-next-13-advanced/tests/main.spec.ts
+++ examples/example-next-13-advanced/tests/main.spec.ts
@@ -345,6 +345,8 @@ it('sets alternate links', async ({request}) => {
         .link.split(', ')
         // On CI, Playwright uses a different host somehow
         .map((cur) => cur.replace(/0\.0\.0\.0/g, 'localhost'))
+        // Normalize ports
+        .map((cur) => cur.replace(/localhost:\d{4}/g, 'localhost:3000'))
     );
   }
 
