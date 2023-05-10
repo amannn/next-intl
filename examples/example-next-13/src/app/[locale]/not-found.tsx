761c2634f5e55dd2ed490a54b3032216b7e6ef59
Jan Amann <jan@amann.work>
2023-05-10 16:11:15 +0200

    Prod CI

--- examples/example-next-13/src/app/[locale]/not-found.tsx
+++ examples/example-next-13/src/app/[locale]/not-found.tsx
@@ -3,8 +3,8 @@
 import {useTranslations} from 'next-intl';
 import PageLayout from 'components/PageLayout';
 
-// Note that `app/[locale]/[...rest]/page.tsx` is
-// necessary for this page to render.
+// Note that `app/[locale]/[...rest]/page.tsx`
+// is necessary for this page to render.
 
 export default function NotFoundPage() {
   const t = useTranslations('NotFoundPage');
