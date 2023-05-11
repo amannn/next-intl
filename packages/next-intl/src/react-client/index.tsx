f26ef999bf92c142d56d0009259e5a224c5dec5b
Jan Amann <jan@amann.work>
2023-05-11 12:15:30 +0200

    fix: Fix support for older Next.js versions by moving `Link` to `next-intl/link` (#288)
    Fixes #287

--- packages/next-intl/src/react-client/index.tsx
+++ packages/next-intl/src/react-client/index.tsx
@@ -2,11 +2,13 @@
  * This is the main entry file when non-'react-server'
  * environments import from 'next-intl'.
  *
- * Make sure this mirrors the API from 'react-server'.
+ * Maintainer notes:
+ * - Make sure this mirrors the API from 'react-server'.
+ * - Make sure everything exported from this module is
+ *   supported in all Next.js versions that are supported.
  */
 
 export * from 'use-intl';
-export {default as Link} from './Link';
 export {default as NextIntlClientProvider} from '../shared/NextIntlClientProvider';
 
 // Legacy export (TBD if we'll deprecate this in favour of `NextIntlClientProvider`)
