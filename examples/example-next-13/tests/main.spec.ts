0a6bce5d57733487b99a7da5037c6195b9d2779b
Jan Amann <jan@amann.work>
2023-05-22 17:31:10 +0200

    fix: Set `SameSite` attribute for locale cookie to `strict` (#302)
    Fixes #301

--- examples/example-next-13/tests/main.spec.ts
+++ examples/example-next-13/tests/main.spec.ts
@@ -39,3 +39,10 @@ it('can be used to localize the page', async ({page}) => {
   await page.goto('/de');
   page.getByRole('heading', {name: 'next-intl Beispiel'});
 });
+
+it('sets a cookie', async ({page}) => {
+  const response = await page.goto('/');
+  expect(await response?.headerValue('set-cookie')).toBe(
+    'NEXT_LOCALE=en; Path=/; SameSite=strict'
+  );
+});
