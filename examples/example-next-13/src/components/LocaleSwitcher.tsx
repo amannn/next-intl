412ac3d555782554f60005d131b29531cdb5d4cf
Jan Amann <jan@amann.work>
2023-05-23 15:38:25 +0200

    docs: Pending state when switching the locale in example-next-13 [skip ci]

--- examples/example-next-13/src/components/LocaleSwitcher.tsx
+++ examples/example-next-13/src/components/LocaleSwitcher.tsx
@@ -1,26 +1,36 @@
 'use client';
 
+import clsx from 'clsx';
 import {useRouter} from 'next/navigation';
 import {useLocale, useTranslations} from 'next-intl';
 import {usePathname} from 'next-intl/client';
-import {ChangeEvent} from 'react';
+import {ChangeEvent, useTransition} from 'react';
 
 export default function LocaleSwitcher() {
   const t = useTranslations('LocaleSwitcher');
+  const [isPending, startTransition] = useTransition();
   const locale = useLocale();
   const router = useRouter();
   const pathname = usePathname();
 
   function onSelectChange(event: ChangeEvent<HTMLSelectElement>) {
-    router.replace(`/${event.target.value}${pathname}`);
+    startTransition(() => {
+      router.replace(`/${event.target.value}${pathname}`);
+    });
   }
 
   return (
-    <label className="relative text-gray-400">
+    <label
+      className={clsx(
+        'relative text-gray-400',
+        isPending && 'transition-opacity [&:disabled]:opacity-30'
+      )}
+    >
       <p className="sr-only">{t('label')}</p>
       <select
         className="inline-flex appearance-none bg-transparent py-3 pl-2 pr-6"
         defaultValue={locale}
+        disabled={isPending}
         onChange={onSelectChange}
       >
         {['en', 'de'].map((cur) => (
