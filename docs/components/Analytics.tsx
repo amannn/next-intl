cae135dd370f7c68bf395fb3ac466846e8186b95
Jan Amann <jan@amann.work>
2023-05-27 10:29:54 +0200

    docs: Remove Vercel Analytics [skip ci]

--- docs/components/Analytics.tsx
+++ docs/components/Analytics.tsx
@@ -1,17 +1,8 @@
-import {Analytics as VercelAnalytics} from '@vercel/analytics/react';
 import Script from 'next/script';
-
-// Currently there are multiple providers being
-// evaluated, ultimately only one should remain.
-// See: https://github.com/umami-software/umami/issues/2051
 
 export default function Analytics() {
   return (
     <>
-      {/* Vercel */}
-      <VercelAnalytics />
-
-      {/* Umami */}
       {process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
         <Script
           async
