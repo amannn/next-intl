c9c2070d18e083684a246185397114119752dc2b
Jan Amann <jan@amann.work>
2023-06-19 15:31:19 +0200

    docs: Static export and update Next.js in examples (#333) [skip ci]
    Closes #334
    Closes #313
    Closes #330

---
+++ examples/example-next-13/src/app/page.tsx
@@ -0,0 +1,6 @@
+import {redirect} from 'next/navigation';
+
+// This page only renders when the app is built statically (output: 'export')
+export default function RootPage() {
+  redirect('/en');
+}
