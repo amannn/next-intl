f6066dcbcceee3ad25a37f2942c9270acaf24397
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-28 16:52:29 +0000

    feat: add prettier config file matching the one from the `docs`. Since You are using spaces for indentation not tabs, and no space inside curly braces, I think this is the best way to go. In general, It's OPEN-SOURCE, so We have to have a consistent config file

---
+++ packages/next-intl/prettier.config.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+/* eslint-disable import/no-extraneous-dependencies */
+
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: false,
+  plugins: [require('prettier-plugin-tailwindcss')]
+};
