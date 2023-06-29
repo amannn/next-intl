2e8e5a8c79aae35a976c887ad661213f9e0d9b07
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-29 00:33:19 +0000

    add useLanguageDirection() to next-13 example

--- examples/example-next-13/src/app/layout.tsx
+++ examples/example-next-13/src/app/layout.tsx
@@ -1,13 +1,19 @@
 import {ReactNode} from 'react';
 import './styles.css';
 
+import {useLocale, useLanguageDirection} from 'next-intl';
+
 type Props = {
   children: ReactNode;
 };
 
-// Even though this component is just passing its children through, the presence
-// of this file fixes an issue in Next.js 13.4 where link clicks that switch
-// the locale would otherwise cause a full reload.
-export default function RootLayout({children}: Props) {
-  return children;
+export default function Layout({children}: Props) {
+  const locale = useLocale();
+  const direction = useLanguageDirection(locale); // 'ltr' or 'rtl'
+
+  return (
+    <html lang={locale} dir={direction}>
+      <body>{children}</body>
+    </html>
+  );
 }
