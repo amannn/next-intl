d404dd1c2e37807e3b65b8486869826782665bad
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-29 00:21:19 +0000

    export the hook

--- packages/use-intl/src/react/index.tsx
+++ packages/use-intl/src/react/index.tsx
@@ -1,6 +1,7 @@
 export {default as IntlProvider} from './IntlProvider';
 export {default as useTranslations} from './useTranslations';
 export {default as useLocale} from './useLocale';
+export {default as useLanguageDirection} from './useLanguageDirection';
 export {default as useNow} from './useNow';
 export {default as useTimeZone} from './useTimeZone';
 export {default as useFormatter} from './useFormatter';
