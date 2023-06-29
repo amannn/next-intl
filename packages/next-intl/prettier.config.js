cab35bec5ade8d05a2bef04a3491c42339c04a8a
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-29 11:59:03 +0000

    remove prettier config files

--- packages/next-intl/prettier.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-/* eslint-disable import/no-extraneous-dependencies */
-
-module.exports = {
-  singleQuote: true,
-  trailingComma: 'none',
-  bracketSpacing: false,
-  plugins: [require('prettier-plugin-tailwindcss')]
-};
