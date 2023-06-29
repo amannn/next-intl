ad95630694dd274c45f762838c3aa302378117a6
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-29 00:34:02 +0000

    add the hook to next-13-next-auth example

--- examples/example-next-13-next-auth/src/app/layout.tsx
+++ examples/example-next-13-next-auth/src/app/layout.tsx
@@ -1,12 +1,17 @@
 import {ReactNode} from 'react';
+import {useLocale, useLanguageDirection} from 'next-intl';
 
 type Props = {
   children: ReactNode;
 };
 
-// Even though this component is just passing its children through, the presence
-// of this file fixes an issue in Next.js 13.3.0 where link clicks that switch
-// the locale would otherwise be ignored.
-export default function RootLayout({children}: Props) {
-  return children;
+export default function Layout({children}: Props) {
+  const locale = useLocale();
+  const direction = useLanguageDirection(locale); // 'ltr' or 'rtl'
+
+  return (
+    <html lang={locale} dir={direction}>
+      <body>{children}</body>
+    </html>
+  );
 }
