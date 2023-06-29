b4bc5d6fa1dc0c203c73780cc930ca0a54877f0e
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-29 00:36:30 +0000

    add tests to language with locale 'en-US', 'ar-SA'

--- packages/use-intl/test/react/useLanguageDirection.test.tsx
+++ packages/use-intl/test/react/useLanguageDirection.test.tsx
@@ -17,4 +17,47 @@ it('returns the language direction', () => {
   screen.getByText('rtl');
 });
 
+// Test ltr language like English
+it('returns the language direction', () => {
+  function Component() {
+    return <>{useLanguageDirection()}</>;
+  }
 
+  render(
+    <IntlProvider locale="en">
+      <Component />
+    </IntlProvider>
+  );
+
+  screen.getByText('ltr');
+});
+
+// Test ltr language like English with region "en-US"
+it('returns the language direction', () => {
+  function Component() {
+    return <>{useLanguageDirection()}</>;
+  }
+
+  render(
+    <IntlProvider locale="en-US">
+      <Component />
+    </IntlProvider>
+  );
+
+  screen.getByText('ltr');
+});
+
+// Test rtl language like Arabic with region "ar-SA"
+it('returns the language direction', () => {
+  function Component() {
+    return <>{useLanguageDirection()}</>;
+  }
+
+  render(
+    <IntlProvider locale="ar-SA">
+      <Component />
+    </IntlProvider>
+  );
+
+  screen.getByText('rtl');
+});
