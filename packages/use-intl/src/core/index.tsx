614cf953f6911dd4c73799ad16fb25b152179198
Jan Amann <jan@amann.me>
2023-07-01 22:56:41 +0200

    Use `tsup` for `use-intl`

--- packages/use-intl/src/core/index.tsx
+++ packages/use-intl/src/core/index.tsx
@@ -1,11 +1,11 @@
-export {default as AbstractIntlMessages} from './AbstractIntlMessages';
-export {
+export type {default as AbstractIntlMessages} from './AbstractIntlMessages';
+export type {
   default as TranslationValues,
   RichTranslationValues
 } from './TranslationValues';
-export {default as Formats} from './Formats';
-export {default as DateTimeFormatOptions} from './DateTimeFormatOptions';
-export {default as NumberFormatOptions} from './NumberFormatOptions';
+export type {default as Formats} from './Formats';
+export type {default as DateTimeFormatOptions} from './DateTimeFormatOptions';
+export type {default as NumberFormatOptions} from './NumberFormatOptions';
 export {default as IntlError, IntlErrorCode} from './IntlError';
 export {default as createTranslator} from './createTranslator';
 export {default as createFormatter} from './createFormatter';
