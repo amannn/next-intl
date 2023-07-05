4d0fefcb558bcee6037dce7bd8cebe727257c8ca
Jan Amann <jan@amann.work>
2023-07-05 18:34:40 +0200

    fix: Bring back ESM build (and fix bundle size) (#381)

--- packages/next-intl/tsup.config.js
+++ packages/next-intl/tsup.config.js
@@ -3,7 +3,7 @@ import {defineConfig} from 'tsup';
 
 export default defineConfig({
   entry: ['src'],
-  format: ['cjs'],
+  format: ['cjs', 'esm'],
   splitting: false,
   sourcemap: true,
   clean: true,
