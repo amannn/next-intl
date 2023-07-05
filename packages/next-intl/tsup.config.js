9ea143dae07c5151b9cad3e29d909c4a01c5e236
Jan Amann <jan@amann.me>
2023-07-05 11:46:26 +0200

    Remove ESM build

--- packages/next-intl/tsup.config.js
+++ packages/next-intl/tsup.config.js
@@ -1,15 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import {defineConfig} from 'tsup';
-import TsupPluginAddRelativeEsmExtensions from '../../scripts/TsupPluginAddRelativeEsmExtensions';
 
 export default defineConfig({
   entry: ['src'],
-  format: ['esm', 'cjs'],
+  format: ['cjs'],
   splitting: false,
   sourcemap: true,
   clean: true,
   bundle: false,
   dts: true,
-  minify: true,
-  plugins: [new TsupPluginAddRelativeEsmExtensions()]
+  minify: true
 });
--- packages/use-intl/tsup.config.js
+++ packages/use-intl/tsup.config.js
@@ -1,17 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import {defineConfig} from 'tsup';
-import TsupPluginAddRelativeEsmExtensions from '../../scripts/TsupPluginAddRelativeEsmExtensions';
 
 const config = {
   entry: ['src'],
-  format: ['esm', 'cjs'],
+  format: ['cjs'],
   splitting: false,
   sourcemap: true,
   clean: true,
   bundle: false,
   dts: true,
-  minify: true,
-  plugins: [new TsupPluginAddRelativeEsmExtensions()]
+  minify: true
 };
 
 export default defineConfig([
