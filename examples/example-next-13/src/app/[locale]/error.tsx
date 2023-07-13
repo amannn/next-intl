3a6f472f7bd73fa6182e8ed5c93e69036bc98bac
Jan Amann <jan@amann.work>
2023-07-13 16:04:56 +0200

    docs: Add error handling docs (#401) [skip ci]
    Fixes #400
    
    Related: https://github.com/amannn/next-intl/discussions/329 #336

---
+++ examples/example-next-13/src/app/[locale]/error.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import {useTranslations} from 'next-intl';
+import {useEffect} from 'react';
+import PageLayout from 'components/PageLayout';
+
+type Props = {
+  error: Error;
+  reset(): void;
+};
+
+export default function Error({error, reset}: Props) {
+  const t = useTranslations('Error');
+
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <PageLayout title={t('title')}>
+      <div>
+        {t.rich('description', {
+          p: (chunks) => <p className="mt-4">{chunks}</p>,
+          retry: (chunks) => (
+            <button
+              className="text-white underline underline-offset-2"
+              onClick={reset}
+              type="button"
+            >
+              {chunks}
+            </button>
+          )
+        })}
+      </div>
+    </PageLayout>
+  );
+}
