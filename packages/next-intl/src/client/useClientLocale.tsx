3323cc2dbb36df46fad5af67c6b0e5d0b8f7759f
Jan Amann <jan@amann.me>
2023-08-01 10:16:39 +0200

    fix: Handle case where the `locale` param is an array

--- packages/next-intl/src/client/useClientLocale.tsx
+++ packages/next-intl/src/client/useClientLocale.tsx
@@ -9,7 +9,7 @@ export default function useClientLocale(): string {
   // `useParams` can be called, but the return type is `null`.
   const params = useParams() as ReturnType<typeof useParams> | null;
 
-  if (params?.[LOCALE_SEGMENT_NAME]) {
+  if (typeof params?.[LOCALE_SEGMENT_NAME] === 'string') {
     locale = params[LOCALE_SEGMENT_NAME];
   } else {
     // eslint-disable-next-line react-hooks/rules-of-hooks -- Reading from context conditionally is fine
