20deaa86dde15121d95742d00261c2dff667a828
Jan Amann <jan@amann.me>
2023-08-21 12:50:30 +0200

    feat: Allow to import `next-intl/link` into Server Components without having to add a wrapping Client Component

---
+++ packages/next-intl/src/link/react-server/Link.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+// Allows to import 'next-intl/link' as a Client Component in 
+// Server Components as a stopgap solution until full RSC 
+// support is available for `next-intl/link`.
+export {default} from '../Link';
