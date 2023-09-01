cddbb4360035aed14bd05a700f8b6a3877e1dd5e
Jan Amann <jan@amann.me>
2023-09-01 10:41:12 +0200

    docs: Fix lint [skip ci]

--- docs/pages/_document.tsx
+++ docs/pages/_document.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import {Html, Head, Main, NextScript} from 'next/document';
 import {SkipNavLink} from 'nextra-theme-docs';
+import React from 'react';
 
 export default function Document() {
   return (
