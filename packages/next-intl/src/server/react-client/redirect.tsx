6c8eb13884959dc9cb5fc980062f529e0ff54718
Jan Amann <jan@amann.me>
2023-09-15 18:42:02 +0200

    Improve error handling when calling `redirect` on the client side

--- packages/next-intl/src/server/react-client/redirect.tsx
+++ packages/next-intl/src/server/react-client/redirect.tsx
@@ -6,7 +6,17 @@ export default function redirect(
   pathname: string,
   ...args: ParametersExceptFirstTwo<typeof baseRedirect>
 ) {
-  // eslint-disable-next-line react-hooks/rules-of-hooks -- Reading from context here is fine, since `redirect` should be called during render
-  const locale = useLocale();
+  let locale;
+  try {
+    // eslint-disable-next-line react-hooks/rules-of-hooks -- Reading from context here is fine, since `redirect` should be called during render
+    locale = useLocale();
+  } catch (e) {
+    throw new Error(
+      process.env.NODE_ENV !== 'production'
+        ? '`redirect()` can only be called during render. To redirect in an event handler or similar, you can use `useRouter()` instead.'
+        : undefined
+    );
+  }
+
   return baseRedirect(pathname, locale, ...args);
 }
