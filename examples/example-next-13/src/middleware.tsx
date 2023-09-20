cd4e5e4863bf1f07fa983cff095a24898c3968d2
Jan Amann <jan@amann.work>
2023-09-20 18:31:35 +0200

    docs: Fix suggested middleware matcher regex and add matcher docs (#505)
    Fixes #504

--- examples/example-next-13/src/middleware.tsx
+++ examples/example-next-13/src/middleware.tsx
@@ -7,5 +7,5 @@ export default createMiddleware({
 
 export const config = {
   // Skip all paths that should not be internationalized
-  matcher: ['/((?!api|_next|.*\\..*).*)']
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
