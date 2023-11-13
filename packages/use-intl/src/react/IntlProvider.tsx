a0a1f045dd6ce1b347a37f38ee39bb1e1aa8987e
Jan Amann <jan@amann.me>
2023-11-13 17:07:28 +0100

    Avoid re-rendering of context consumers if possible

--- packages/use-intl/src/react/IntlProvider.tsx
+++ packages/use-intl/src/react/IntlProvider.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode, useState} from 'react';
+import React, {ReactNode, useMemo, useState} from 'react';
 import IntlConfig from '../core/IntlConfig';
 import initializeConfig from '../core/initializeConfig';
 import IntlContext from './IntlContext';
@@ -7,17 +7,53 @@ type Props = IntlConfig & {
   children: ReactNode;
 };
 
-export default function IntlProvider({children, ...config}: Props) {
+export default function IntlProvider({
+  children,
+  defaultTranslationValues,
+  formats,
+  getMessageFallback,
+  locale,
+  messages,
+  now,
+  onError,
+  timeZone
+}: Props) {
   const [messageFormatCache] = useState(() => new Map());
 
-  return (
-    <IntlContext.Provider
-      value={{
-        ...initializeConfig(config),
-        messageFormatCache
-      }}
-    >
-      {children}
-    </IntlContext.Provider>
+  // Memoizing this value helps to avoid triggering a re-render of all
+  // context consumers in case the configuration didn't change. However,
+  // if some of the non-primitive values change, a re-render will still
+  // be triggered. Note that there's no need to put `memo` on `IntlProvider`
+  // itself, because the `children` typically change on every render.
+  // There's some burden on the consumer side if it's important to reduce
+  // re-renders, put that's how React works.
+  // See: https://blog.isquaredsoftware.com/2020/05/blogged-answers-a-mostly-complete-guide-to-react-rendering-behavior/#context-updates-and-render-optimizations
+  const value = useMemo(
+    () => ({
+      ...initializeConfig({
+        locale,
+        defaultTranslationValues,
+        formats,
+        getMessageFallback,
+        messages,
+        now,
+        onError,
+        timeZone
+      }),
+      messageFormatCache
+    }),
+    [
+      defaultTranslationValues,
+      formats,
+      getMessageFallback,
+      locale,
+      messageFormatCache,
+      messages,
+      now,
+      onError,
+      timeZone
+    ]
   );
+
+  return <IntlContext.Provider value={value}>{children}</IntlContext.Provider>;
 }
