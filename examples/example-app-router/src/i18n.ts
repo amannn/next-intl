f009a5e22cc33e0bdda450d7e40be7f227a096b6
Jan Amann <jan@amann.me>
2023-11-17 14:43:30 +0100

    Fix quotes

--- examples/example-app-router/src/i18n.ts
+++ examples/example-app-router/src/i18n.ts
@@ -4,7 +4,7 @@ export default getRequestConfig(async ({locale}) => ({
   messages: (
     await (locale === 'en'
       ? // When using Turbopack, this will enable HMR for `en`
-        import(`../messages/en.json`)
+        import('../messages/en.json')
       : import(`../messages/${locale}.json`))
   ).default
 }));
