bb488efeec519677a2a7d607dbff2962a9c90294
Jan Amann <jan@amann.me>
2023-11-23 11:45:03 +0100

    docs: Fix base url

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  baseUrl: process.env.VERCEL_URL
-    ? 'https://' + process.env.VERCEL_URL
-    : 'https://next-intl-docs.vercel.app',
+  baseUrl: 'https://next-intl-docs.vercel.app',
   githubUrl: 'https://github.com/amannn/next-intl'
 };
