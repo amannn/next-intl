7036dbf5a92f4937f53acf45537919799a9e578b
adrigar94 <112860345+adrigar94@users.noreply.github.com>
2023-12-04 17:17:47 +0100

    Improve route matching in middleware

--- examples/example-app-router/src/middleware.ts
+++ examples/example-app-router/src/middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(de|en)/:path*']
+  matcher: ['/', '/('+locales.join('|')+')/:path*']
 };
