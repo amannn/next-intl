e00ed97746dc8d6c264b2f4aa9162681c1a89919
Jan Amann <jan@amann.work>
2023-12-05 10:54:49 +0100

    fix: Prefer `locale` from `NextIntlClientProvider` when using `useLocale` in Client Components. `useParams().locale` is still used as a fallback. (#691)
    Fixes #688

--- packages/next-intl/test/react-client/useLocale.test.tsx
+++ packages/next-intl/test/react-client/useLocale.test.tsx
@@ -1,17 +1,34 @@
 import {render, screen} from '@testing-library/react';
+import {useParams} from 'next/navigation';
 import React from 'react';
-import {it, vi} from 'vitest';
-import {useLocale} from '../../src/index.react-client';
+import {expect, it, vi} from 'vitest';
+import {NextIntlClientProvider, useLocale} from '../../src/index.react-client';
 
 vi.mock('next/navigation', () => ({
-  useParams: () => ({locale: 'en'})
+  useParams: vi.fn(() => ({locale: 'en'}))
 }));
 
-it('returns a locale from `useParams` without a provider', () => {
-  function Component() {
-    return <>{useLocale()}</>;
-  }
+function Component() {
+  return <>{useLocale()}</>;
+}
 
+it('returns a locale from `useParams` without a provider', () => {
   render(<Component />);
   screen.getByText('en');
+});
+
+it('prioritizes the locale from the provider', () => {
+  render(
+    <NextIntlClientProvider locale="de">
+      <Component />
+    </NextIntlClientProvider>
+  );
+  screen.getByText('de');
+});
+
+it('throws if neither a locale from the provider or useParams is available', () => {
+  vi.mocked(useParams).mockImplementation(() => ({}));
+  expect(() => render(<Component />)).toThrow(
+    'No intl context found. Have you configured the provider?'
+  );
 });
