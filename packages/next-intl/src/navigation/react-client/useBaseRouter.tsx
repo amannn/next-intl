a392dfd34eb9eda62104e072019961604a46e368
Jan Amann <jan@amann.me>
2024-01-18 16:12:02 +0100

    Sync cookie in useRouter

--- packages/next-intl/src/navigation/react-client/useBaseRouter.tsx
+++ packages/next-intl/src/navigation/react-client/useBaseRouter.tsx
@@ -1,6 +1,11 @@
-import {useRouter as useNextRouter} from 'next/navigation';
+import {useRouter as useNextRouter, usePathname} from 'next/navigation';
 import {useMemo} from 'react';
 import useLocale from '../../react-client/useLocale';
+import {
+  COOKIE_LOCALE_NAME,
+  COOKIE_MAX_AGE,
+  COOKIE_SAME_SITE
+} from '../../shared/constants';
 import {AllLocales} from '../../shared/types';
 import {localizeHref} from '../../shared/utils';
 
@@ -30,6 +35,7 @@ type IntlNavigateOptions<Locales extends AllLocales> = {
 export default function useBaseRouter<Locales extends AllLocales>() {
   const router = useNextRouter();
   const locale = useLocale();
+  const pathname = usePathname();
 
   return useMemo(() => {
     function localize(href: string, nextLocale?: string) {
@@ -41,14 +47,30 @@ export default function useBaseRouter<Locales extends AllLocales>() {
       );
     }
 
-    return {
-      ...router,
-      push(
+    function createHandler<
+      Options,
+      Fn extends (href: string, options?: Options) => void
+    >(fn: Fn) {
+      return function handler(
         href: string,
-        options?: Parameters<typeof router.push>[1] &
-          IntlNavigateOptions<Locales>
-      ) {
+        options?: Options & IntlNavigateOptions<Locales>
+      ): void {
         const {locale: nextLocale, ...rest} = options || {};
+
+        // We have to keep the cookie value in sync as Next.js might
+        // skip a request to the server due to its router cache.
+        // See https://github.com/amannn/next-intl/issues/786.
+        const isSwitchingLocale = nextLocale !== locale;
+        if (isSwitchingLocale) {
+          const basePath = window.location.pathname.replace(pathname, '');
+          const hasBasePath = basePath !== '';
+          const path = hasBasePath ? basePath : '/';
+
+          // Note that writing to `document.cookie` doesn't overwrite all
+          // cookies, but only the ones referenced via the name here.
+          document.cookie = `${COOKIE_LOCALE_NAME}=${nextLocale}; path=${path}; max-age=${COOKIE_MAX_AGE}; sameSite=${COOKIE_SAME_SITE}`;
+        }
+
         const args: [
           href: string,
           options?: Parameters<typeof router.push>[1]
@@ -56,41 +78,26 @@ export default function useBaseRouter<Locales extends AllLocales>() {
         if (Object.keys(rest).length > 0) {
           args.push(rest);
         }
-        return router.push(...args);
-      },
 
-      replace(
-        href: string,
-        options?: Parameters<typeof router.replace>[1] &
-          IntlNavigateOptions<Locales>
-      ) {
-        const {locale: nextLocale, ...rest} = options || {};
-        const args: [
-          href: string,
-          options?: Parameters<typeof router.replace>[1]
-        ] = [localize(href, nextLocale)];
-        if (Object.keys(rest).length > 0) {
-          args.push(rest);
-        }
-        return router.replace(...args);
-      },
+        // @ts-expect-error -- This is ok
+        return fn(...args);
+      };
+    }
 
-      prefetch(
-        href: string,
-        options?: Parameters<typeof router.prefetch>[1] &
-          IntlNavigateOptions<Locales>
-      ) {
-        const {locale: nextLocale, ...rest} = options || {};
-        const args: [
-          href: string,
-          options?: Parameters<typeof router.prefetch>[1]
-        ] = [localize(href, nextLocale)];
-        if (Object.keys(rest).length > 0) {
-          // @ts-expect-error TypeScript thinks `rest` can be an empty object
-          args.push(rest);
-        }
-        return router.prefetch(...args);
-      }
+    return {
+      ...router,
+      push: createHandler<
+        Parameters<typeof router.push>[1],
+        typeof router.push
+      >(router.push),
+      replace: createHandler<
+        Parameters<typeof router.replace>[1],
+        typeof router.replace
+      >(router.replace),
+      prefetch: createHandler<
+        Parameters<typeof router.prefetch>[1],
+        typeof router.prefetch
+      >(router.prefetch)
     };
-  }, [locale, router]);
+  }, [locale, pathname, router]);
 }
--- packages/next-intl/src/shared/constants.tsx
+++ packages/next-intl/src/shared/constants.tsx
@@ -1,6 +1,10 @@
 // Reuse the legacy cookie name
 // https://nextjs.org/docs/advanced-features/i18n-routing#leveraging-the-next_locale-cookie
 export const COOKIE_LOCALE_NAME = 'NEXT_LOCALE';
+export const COOKIE_MAX_AGE = 31536000; // 1 year
+export const COOKIE_SAME_SITE = 'strict';
+
+export const COOKIE_BASE_PATH_NAME = 'NEXT_INTL_BASE_PATH';
 
 // Should take precedence over the cookie
 export const HEADER_LOCALE_NAME = 'X-NEXT-INTL-LOCALE';
