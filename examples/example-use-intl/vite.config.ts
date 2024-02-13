90cc2a0fac341c0f23119e74b9bc85863252700a
Jan Amann <jan@amann.me>
2024-02-13 14:04:37 +0100

    Fix Vite config and remove ESM exports

--- examples/example-use-intl/vite.config.ts
+++ examples/example-use-intl/vite.config.ts
@@ -2,5 +2,13 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['use-intl']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/use-intl/, /node_modules/]
+    }
+  }
 });
