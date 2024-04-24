2375929361baa2c7a7c78e2f327c6445eaacbf36
Jan Amann <jan@amann.me>
2024-04-24 20:45:24 +0200

    Fix types

--- packages/next-intl/src/server/react-server/getRequestConfig.tsx
+++ packages/next-intl/src/server/react-server/getRequestConfig.tsx
@@ -1,6 +1,16 @@
 import type {IntlConfig} from 'use-intl/core';
 
-type RequestConfig = Omit<IntlConfig, 'locale'>;
+type RequestConfig = Omit<IntlConfig, 'locale'> & {
+  /**
+   * Instead of reading a `locale` from the argument that's passed to the
+   * function within `getRequestConfig`, you can include a locale as part of the
+   * returned request configuration.
+   *
+   * This is helpful for apps that only support a single language and for apps
+   * where the locale should be read from user settings instead of the pathname.
+   **/
+  locale?: IntlConfig['locale'];
+};
 
 export type GetRequestConfigParams = {
   locale: string;
