a3a4ccf65c67e576e6850fa0956c84a294b1e38d
Jan Amann <jan@amann.me>
2024-05-24 09:53:55 +0200

    Fix imports

--- examples/example-app-router-playground/src/components/NavigationLink.tsx
+++ examples/example-app-router-playground/src/components/NavigationLink.tsx
@@ -2,7 +2,8 @@
 
 import {useSelectedLayoutSegment} from 'next/navigation';
 import {ComponentProps} from 'react';
-import {Link, pathnames} from '../navigation';
+import {pathnames} from '../config';
+import {Link} from '../navigation';
 
 export default function NavigationLink<
   Pathname extends keyof typeof pathnames
