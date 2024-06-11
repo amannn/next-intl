b7480bf6e185454dd156457a4c54ff02ff3d265c
Jan Amann <jan@amann.me>
2024-06-11 12:53:52 +0200

    Test tree-shaking of parser

--- packages/use-intl/.size-limit.ts
+++ packages/use-intl/.size-limit.ts
@@ -2,10 +2,19 @@ import type {SizeLimitConfig} from 'size-limit';
 
 const config: SizeLimitConfig = [
   {
+    name: 'All (ESM)',
     path: 'dist/index.mjs',
     limit: '13.16 kB'
   },
   {
+    name: 'All but useTranslations (ESM)',
+    path: 'dist/index.mjs',
+    import:
+      '{useLocale, IntlProvider, useNow, useTimeZone, useMessages, useFormatter}',
+    limit: '1.58 kB'
+  },
+  {
+    name: 'All (CJS)',
     path: 'dist/index.js',
     limit: '16.81 kB'
   }
