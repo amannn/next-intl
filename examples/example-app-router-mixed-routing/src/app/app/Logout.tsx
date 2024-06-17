7e00cdf7cd4910013096fcc763fda488dc395290
Jan Amann <jan@amann.me>
2024-06-17 10:52:48 +0200

    docs: Update mixed routing example to use cookie for logged in user

--- examples/example-app-router-mixed-routing/src/app/app/Logout.tsx
+++ examples/example-app-router-mixed-routing/src/app/app/Logout.tsx
@@ -1,10 +1,7 @@
 import Link from 'next/link';
-import {useLocale, useTranslations} from 'next-intl';
+import {useTranslations} from 'next-intl';
 
 export default function Logout() {
   const t = useTranslations('Logout');
-  const locale = useLocale();
-
-  // Redirect to the locale preference of the user
-  return <Link href={'/' + locale}>{t('label')}</Link>;
+  return <Link href="/">{t('label')}</Link>;
 }
