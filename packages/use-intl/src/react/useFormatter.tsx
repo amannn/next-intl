fc536aee692075c18bf2921aa4b9f6018173f0c1
Jan Amann <jan@amann.me>
2024-06-19 14:50:57 +0200

    fix: Remove `@formatjs/ecma402-abstract` dependency in favor of the automatically bundled one from `intl-messageformat`

--- packages/use-intl/src/react/useFormatter.tsx
+++ packages/use-intl/src/react/useFormatter.tsx
@@ -2,7 +2,7 @@ import {useMemo} from 'react';
 import createFormatter from '../core/createFormatter';
 import useIntlContext from './useIntlContext';
 
-export default function useFormatter() {
+export default function useFormatter(): ReturnType<typeof createFormatter> {
   const {formats, locale, now: globalNow, onError, timeZone} = useIntlContext();
 
   return useMemo(
