2ffcb7af57f6c45a57229aa3f788a4334171edaa
Jan Amann <jan@amann.work>
2024-06-25 18:36:14 +0200

    docs: Improve phrasing for Algolia index (#1149)

--- docs/components/Example.tsx
+++ docs/components/Example.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 type Props = {
   demoLink?: string;
   description: string;
-  hash: string;
+  id: string;
   image?: string;
   name: string;
   sourceLink: string;
@@ -13,16 +13,16 @@ type Props = {
 export default function Example({
   demoLink,
   description,
-  hash,
+  id,
   image,
   name,
   sourceLink
 }: Props) {
   const locationHash = useLocationHash();
-  const isActive = locationHash === hash;
+  const isActive = locationHash === id;
 
   return (
-    <div className="relative max-w-lg scroll-m-4" id={hash}>
+    <div className="relative max-w-lg">
       {isActive && (
         <div className="absolute -left-6 top-0 h-full w-1 bg-sky-200 dark:bg-sky-900" />
       )}
@@ -38,37 +38,47 @@ export default function Example({
           <div className="absolute inset-0 shadow-[inset_0_0_50px_0_rgba(0,0,0,0.04)]" />
         </div>
       )}
-      <h2 className="flex scroll-mt-8 items-center text-xl font-semibold">
-        {name}
-      </h2>
-      <div className="mt-2">
-        <p className="mt-2 max-w-lg text-base text-slate-600 dark:text-slate-400">
-          {description}
-        </p>
+      <div className="relative">
+        <a
+          aria-label="Permalink for this example"
+          className="subheading-anchor absolute right-0 top-0 scroll-m-[24rem]"
+          href={`#${id}`}
+          id={id}
+        >
+          {/* Styled by nextra via CSS class */}
+        </a>
+        <h2 className="flex scroll-mt-8 items-center text-xl font-semibold">
+          {name}
+        </h2>
         <div className="mt-2">
-          <a
-            className="nx-text-primary-600 inline-block text-base underline"
-            href={sourceLink}
-            rel="noreferrer"
-            target="_blank"
-          >
-            Source
-          </a>
-          {demoLink && (
-            <>
-              <span className="inline-block text-base text-slate-500">
-                {' ・ '}
-              </span>
-              <a
-                className="nx-text-primary-600 inline-block text-base underline"
-                href={demoLink}
-                rel="noreferrer"
-                target="_blank"
-              >
-                Demo
-              </a>
-            </>
-          )}
+          <p className="mt-2 max-w-lg text-base text-slate-600 dark:text-slate-400">
+            {description}
+          </p>
+          <div className="mt-2">
+            <a
+              className="nx-text-primary-600 inline-block text-base underline"
+              href={sourceLink}
+              rel="noreferrer"
+              target="_blank"
+            >
+              Source
+            </a>
+            {demoLink && (
+              <>
+                <span className="inline-block text-base text-slate-500">
+                  {' ・ '}
+                </span>
+                <a
+                  className="nx-text-primary-600 inline-block text-base underline"
+                  href={demoLink}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Demo
+                </a>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
