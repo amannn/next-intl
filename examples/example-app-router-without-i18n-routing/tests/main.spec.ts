b1385407dd1e71d3c61dc51e549b3bc29cfac9d0
Jan Amann <jan@amann.me>
2024-06-25 11:52:08 +0200

    Fix test

--- examples/example-app-router-without-i18n-routing/tests/main.spec.ts
+++ examples/example-app-router-without-i18n-routing/tests/main.spec.ts
@@ -1,29 +1,27 @@
-import {Page, test as it} from '@playwright/test';
-
-async function submitLoginForm(page: Page, password: string) {
-  await page.getByRole('textbox', {name: 'Email'}).fill('jane@doe.com');
-  await page.getByRole('textbox', {name: 'Password'}).fill(password);
-  await page.getByRole('textbox', {name: 'Password'}).press('Enter');
-}
+import {test as it} from '@playwright/test';
 
 it('shows localized server errors in the login form', async ({page}) => {
   await page.goto('/login');
-  await submitLoginForm(page, 'invalid');
+  await page.getByRole('textbox', {name: 'Email'}).fill('jane@doe.com');
+  await page.getByRole('textbox', {name: 'Password'}).fill('invalid');
+  await page.getByRole('textbox', {name: 'Password'}).press('Enter');
   await page.getByText('Please check your credentials.').isVisible();
 
-  await page
-    .getByRole('combobox', {name: 'Language'})
-    .selectOption({label: 'Deutsch'});
-  await submitLoginForm(page, 'invalid');
+  await page.getByRole('combobox', {name: 'Language'}).click();
+  await page.getByRole('option', {name: 'Deutsch'}).click();
+  await page.getByRole('textbox', {name: 'Email'}).fill('jane@doe.com');
+  await page.getByRole('textbox', {name: 'Passwort'}).fill('invalid');
+  await page.getByRole('textbox', {name: 'Passwort'}).press('Enter');
   await page.getByText('Bitte überprüfen Sie Ihre Anmeldedaten.').isVisible();
 });
 
 it('can login and switch the language', async ({page}) => {
   await page.goto('/login');
-  await submitLoginForm(page, 'password');
+  await page.getByRole('textbox', {name: 'Email'}).fill('jane@doe.com');
+  await page.getByRole('textbox', {name: 'Password'}).fill('password');
+  await page.getByRole('textbox', {name: 'Password'}).press('Enter');
   await page.getByRole('heading', {name: 'Start'}).isVisible();
-  await page
-    .getByRole('combobox', {name: 'Language'})
-    .selectOption({label: 'English'});
+  await page.getByRole('combobox', {name: 'Language'}).click();
+  await page.getByRole('option', {name: 'English'}).click();
   await page.getByRole('heading', {name: 'Home'}).isVisible();
 });
