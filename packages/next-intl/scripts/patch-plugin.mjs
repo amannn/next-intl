7487041fd763548a98f4e4be1e4a92f61f19892b
Jan Amann <jan@amann.me>
2024-06-26 14:32:18 +0200

    Patch plugin

---
+++ packages/next-intl/scripts/patch-plugin.mjs
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+function patchFile(filePath, oldString, newString) {
+  const data = fs.readFileSync(filePath, 'utf8');
+  const updatedData = data.replace(oldString, newString);
+  fs.writeFileSync(filePath, updatedData, 'utf8');
+}
+
+patchFile(
+  './dist/cjs/plugin.js',
+  'exports.default = createNextIntlPlugin;',
+  'module.exports = createNextIntlPlugin;module.exports.default = createNextIntlPlugin;'
+);
+
+patchFile(
+  './dist/cjs/plugin.d.ts',
+  'export { createNextIntlPlugin as default };',
+  'export = createNextIntlPlugin;'
+);
+
+console.log('CJS entry for ./plugin patched');
