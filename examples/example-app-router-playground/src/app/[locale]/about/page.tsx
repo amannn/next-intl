1b35232eb048496165d4d88052d516ed91684be5
Jan Amann <jan@amann.me>
2024-07-09 18:13:46 +0200

    MDX docs and example

---
+++ examples/example-app-router-playground/src/app/[locale]/about/page.tsx
@@ -0,0 +1,21 @@
+import fs from 'fs/promises';
+import {EvaluateOptions, evaluate} from '@mdx-js/mdx';
+import {getLocale} from 'next-intl/server';
+import * as runtime from 'react/jsx-runtime';
+import AsyncComponent from '../../../components/AsyncComponent';
+import Counter from '../../../components/client/02-MessagesOnClientCounter/Counter';
+
+async function MDXContent({filename}: {filename: string}) {
+  // Custom components that can be
+  // referenced in the content
+  const components = {AsyncComponent, Counter};
+
+  const locale = await getLocale();
+  const file = await fs.readFile(`./content/${locale}/${filename}`, 'utf-8');
+  const {default: Content} = await evaluate(file, runtime as EvaluateOptions);
+  return <Content components={components} />;
+}
+
+export default async function AboutPage() {
+  return <MDXContent filename="about.mdx" />;
+}
