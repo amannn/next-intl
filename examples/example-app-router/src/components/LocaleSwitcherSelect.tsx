74724db70ef949fd34df6bddc8c3a6bcfaab869f
Jan Amann <jan@amann.me>
2024-08-14 13:55:10 +0200

    docs: Adjust locale switcher example to provide more type safety

--- examples/example-app-router/src/components/LocaleSwitcherSelect.tsx
+++ examples/example-app-router/src/components/LocaleSwitcherSelect.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx';
 import {useParams} from 'next/navigation';
 import {ChangeEvent, ReactNode, useTransition} from 'react';
 import {useRouter, usePathname} from '@/navigation';
+import {Locale} from '@/types';
 
 type Props = {
   children: ReactNode;
@@ -22,13 +23,16 @@ export default function LocaleSwitcherSelect({
   const params = useParams();
 
   function onSelectChange(event: ChangeEvent<HTMLSelectElement>) {
-    const nextLocale = event.target.value;
+    const nextLocale = event.target.value as Locale;
     startTransition(() => {
       router.replace(
-        // @ts-expect-error -- TypeScript will validate that only known `params`
-        // are used in combination with a given `pathname`. Since the two will
-        // always match for the current route, we can skip runtime checks.
-        {pathname, params},
+        {
+          pathname,
+          // @ts-expect-error -- TypeScript will validate that only known `params`
+          // are used in combination with a given `pathname`. Since the two will
+          // always match for the current route, we can skip runtime checks.
+          params
+        },
         {locale: nextLocale}
       );
     });
