1bd7ea1c9bc9bd3c28f433fb0d5c29be856b54b1
Jan Amann <jan@amann.me>
2024-08-28 12:26:06 +0200

    Migrate example-app-router-next-auth

--- examples/example-app-router-next-auth/src/navigation.ts
+++ examples/example-app-router-next-auth/src/navigation.ts
@@ -1,5 +1,11 @@
 import {createSharedPathnamesNavigation} from 'next-intl/navigation';
+import {defineRouting} from 'next-intl/routing';
 
-export const locales = ['en', 'de'] as const;
+export const routing = defineRouting({
+  locales: ['en', 'de'],
+  defaultLocale: 'en',
+  localePrefix: 'as-needed'
+});
+
 export const {Link, redirect, usePathname, useRouter} =
-  createSharedPathnamesNavigation({locales});
+  createSharedPathnamesNavigation(routing);
