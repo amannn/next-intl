bf6d2ad0eec6778c09d15a0b7c576ea5d95ef397
Jan Amann <jan@amann.work>
2024-09-24 11:36:09 +0200

    fix: Make all keys of `Formats` type optional for easier usage

--- packages/use-intl/src/core/Formats.tsx
+++ packages/use-intl/src/core/Formats.tsx
@@ -2,9 +2,9 @@ import DateTimeFormatOptions from './DateTimeFormatOptions';
 import NumberFormatOptions from './NumberFormatOptions';
 
 type Formats = {
-  number: Record<string, NumberFormatOptions>;
-  dateTime: Record<string, DateTimeFormatOptions>;
-  list: Record<string, Intl.ListFormatOptions>;
+  number?: Record<string, NumberFormatOptions>;
+  dateTime?: Record<string, DateTimeFormatOptions>;
+  list?: Record<string, Intl.ListFormatOptions>;
 };
 
 export default Formats;
