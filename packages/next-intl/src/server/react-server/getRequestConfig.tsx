15358fa7a76b786724c1c92978551c9ce2a2d304
Jan Amann <jan@amann.work>
2024-10-02 16:24:37 +0200

    deprecate locale and adjust troubleshooting docs

--- packages/next-intl/src/server/react-server/getRequestConfig.tsx
+++ packages/next-intl/src/server/react-server/getRequestConfig.tsx
@@ -16,10 +16,13 @@ export type RequestConfig = Omit<IntlConfig, 'locale'> & {
 
 export type GetRequestConfigParams = {
   /**
-   * The locale that was matched by the `[locale]` path segment.
+   * Deprecated in favor of `requestLocale` (see https://github.com/amannn/next-intl/pull/1383).
    *
-   * Note however that this can be overridden in async APIs when the `locale`
-   * is explicitly passed (e.g. `getTranslations({locale: 'en'})`).
+   * The locale that was matched by the `[locale]` path segment. Note however
+   * that this can be overridden in async APIs when the `locale` is explicitly
+   * passed (e.g. `getTranslations({locale: 'en'})`).
+   *
+   * @deprecated
    */
   locale: string;
 
