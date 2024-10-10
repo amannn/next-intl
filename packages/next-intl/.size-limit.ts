42125466765889bdd28c2e0de7d0a53a4cb45e0d
Jan Amann <jan@amann.work>
2024-10-10 16:34:36 +0200

    bump size

--- packages/next-intl/.size-limit.ts
+++ packages/next-intl/.size-limit.ts
@@ -60,7 +60,7 @@ const config: SizeLimitConfig = [
   {
     name: 'import createMiddleware from \'next-intl/middleware\'',
     path: 'dist/production/middleware.js',
-    limit: '9.63 KB'
+    limit: '9.665 KB'
   },
   {
     name: 'import * from \'next-intl/routing\'',
