578928284d54cb0caf9af155255daa5a72412733
Jan Amann <jan@amann.work>
2024-10-16 16:09:18 +0200

    fix mdx rendering

--- examples/example-app-router-playground/next.config.mjs
+++ examples/example-app-router-playground/next.config.mjs
@@ -1,21 +1,25 @@
 // @ts-check
 
-import mdxPlugin from '@next/mdx';
+import createMDX from '@next/mdx';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.tsx');
-const withMdx = mdxPlugin();
+const withMdx = createMDX({});
 
-export default withMdx(
-  withNextIntl({
-    trailingSlash: process.env.NEXT_PUBLIC_USE_CASE === 'trailing-slash',
-    basePath: process.env.NEXT_PUBLIC_USE_CASE === 'base-path' ? '/base/path' : undefined,
-    experimental: {
-      staleTimes: {
-        // Next.js 14.2 broke `locale-prefix-never.spec.ts`.
-        // This is a workaround for the time being.
-        dynamic: 0
-      }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  trailingSlash: process.env.NEXT_PUBLIC_USE_CASE === 'trailing-slash',
+  basePath:
+    process.env.NEXT_PUBLIC_USE_CASE === 'base-path' ? '/base/path' : undefined,
+  experimental: {
+    staleTimes: {
+      // Next.js 14.2 broke `locale-prefix-never.spec.ts`.
+      // This is a workaround for the time being.
+      dynamic: 0
     }
-  })
-);
+  }
+};
+
+export default withNextIntl(withMdx(nextConfig));
