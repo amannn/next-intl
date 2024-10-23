1fae75b155ac6634e2cbacfc6e0d65719ef000e9
Jan Amann <jan@amann.work>
2024-10-23 15:33:40 +0200

    docs: Move to `src` (#1453)

---
+++ docs/src/pages/blog/_meta.tsx
@@ -0,0 +1,21 @@
+export default {
+  index: {
+    title: 'Overview'
+  },
+  'next-intl-3-22': {
+    title: 'next-intl 3.22',
+    display: 'hidden'
+  },
+  'date-formatting-nextjs': {
+    title: 'Reliable date formatting in Next.js',
+    display: 'hidden'
+  },
+  'next-intl-3-0': {
+    title: 'next-intl 3.0',
+    display: 'hidden'
+  },
+  'translations-outside-of-react-components': {
+    title: 'How (not) to use translations outside of React components',
+    display: 'hidden'
+  }
+};
