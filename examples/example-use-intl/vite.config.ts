212e412db1ddce84b8de2ab04d71ff96d64e720f
Jan Amann <jan@amann.work>
2024-10-28 16:26:20 +0100

    remove vite workaround

--- examples/example-use-intl/vite.config.ts
+++ examples/example-use-intl/vite.config.ts
@@ -1,11 +1,6 @@
-import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-
-  // TODO: Remove after use-intl has full ESM support
-  // https://vitejs.dev/guide/dep-pre-bundling#monorepos-and-linked-dependencies
-  optimizeDeps: {include: ['use-intl']},
-  build: {commonjsOptions: {include: [/use-intl/, /node_modules/]}}
+  plugins: [react()]
 });
