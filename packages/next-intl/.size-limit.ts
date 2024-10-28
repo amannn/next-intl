b3a161f65e054a2ef7149fae57a133b705c1bdd5
Jan Amann <jan@amann.work>
2024-10-28 17:35:39 +0100

    decrease sizes

--- packages/next-intl/.size-limit.ts
+++ packages/next-intl/.size-limit.ts
@@ -4,30 +4,30 @@ const config: SizeLimitConfig = [
   {
     name: "import * from 'next-intl' (react-client, production)",
     path: 'dist/esm/production/index.react-client.js',
-    limit: '14.125 KB'
+    limit: '13.195 KB'
   },
   {
     name: "import {NextIntlClientProvider} from 'next-intl' (react-client, production)",
     import: '{NextIntlClientProvider}',
     path: 'dist/esm/production/index.react-client.js',
-    limit: '1.55 KB'
+    limit: '1.035 KB'
   },
   {
     name: "import * from 'next-intl' (react-server, production)",
     path: 'dist/esm/production/index.react-server.js',
-    limit: '14.135 KB'
+    limit: '14.125 KB'
   },
   {
     name: "import {createNavigation} from 'next-intl/navigation' (react-client, production)",
     path: 'dist/esm/production/navigation.react-client.js',
     import: '{createNavigation}',
-    limit: '4.045 KB'
+    limit: '2.525 KB'
   },
   {
     name: "import {createNavigation} from 'next-intl/navigation' (react-server, production)",
     path: 'dist/esm/production/navigation.react-server.js',
     import: '{createNavigation}',
-    limit: '4.045 KB'
+    limit: '3.425 KB'
   },
   {
     name: "import * from 'next-intl/server' (react-client, production)",
@@ -37,12 +37,12 @@ const config: SizeLimitConfig = [
   {
     name: "import * from 'next-intl/server' (react-server, production)",
     path: 'dist/esm/production/server.react-server.js',
-    limit: '14.035 KB'
+    limit: '13.385 KB'
   },
   {
     name: "import * from 'next-intl/middleware' (production)",
     path: 'dist/esm/production/middleware.js',
-    limit: '9.725 KB'
+    limit: '9.295 KB'
   },
   {
     name: "import * from 'next-intl/routing' (production)",
--- packages/use-intl/.size-limit.ts
+++ packages/use-intl/.size-limit.ts
@@ -5,20 +5,20 @@ const config: SizeLimitConfig = [
     name: "import * from 'use-intl' (production)",
     import: '*',
     path: 'dist/esm/production/index.js',
-    limit: '14.195 kB'
+    limit: '13.075 kB'
   },
   {
     name: "import {IntlProvider, useLocale, useNow, useTimeZone, useMessages, useFormatter} from 'use-intl' (production)",
     path: 'dist/esm/production/index.js',
     import:
       '{IntlProvider, useLocale, useNow, useTimeZone, useMessages, useFormatter}',
-    limit: '2.935 kB'
+    limit: '1.985 kB'
   },
   {
     name: "import * from 'use-intl' (development)",
     import: '*',
     path: 'dist/esm/development/index.js',
-    limit: '14.195 kB'
+    limit: '13.955 kB'
   }
 ];
 
