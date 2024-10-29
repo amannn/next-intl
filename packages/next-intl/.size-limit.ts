49495838c3f3040847f426084c93253d9e804a36
Jan Amann <jan@amann.work>
2024-10-29 15:30:40 +0100

    bump sizes

--- packages/next-intl/.size-limit.ts
+++ packages/next-intl/.size-limit.ts
@@ -33,7 +33,7 @@ const config: SizeLimitConfig = [
     name: "import {createSharedPathnamesNavigation} from 'next-intl/navigation' (react-server)",
     path: 'dist/production/navigation.react-server.js',
     import: '{createSharedPathnamesNavigation}',
-    limit: '16.795 KB'
+    limit: '16.825 KB'
   },
   {
     name: "import {createLocalizedPathnamesNavigation} from 'next-intl/navigation' (react-server)",
@@ -45,7 +45,7 @@ const config: SizeLimitConfig = [
     name: "import {createNavigation} from 'next-intl/navigation' (react-server)",
     path: 'dist/production/navigation.react-server.js',
     import: '{createNavigation}',
-    limit: '16.8 KB'
+    limit: '16.815 KB'
   },
   {
     name: "import * from 'next-intl/server' (react-client)",
