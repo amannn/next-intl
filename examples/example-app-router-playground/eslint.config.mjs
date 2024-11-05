b1e8fa804356c76e2df0bce94e008da9c689fa48
Jan Amann <jan@amann.work>
2024-11-05 15:06:56 +0100

    use plugin in playground

--- examples/example-app-router-playground/eslint.config.mjs
+++ examples/example-app-router-playground/eslint.config.mjs
@@ -1,3 +1,8 @@
 import {getPresets} from 'eslint-config-molindo';
+import globals from 'globals';
 
-export default await getPresets('typescript', 'react', 'jest');
+export default (await getPresets('typescript', 'react', 'jest')).concat({
+  languageOptions: {
+    globals: globals.node
+  }
+});
