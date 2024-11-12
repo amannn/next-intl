096033a7265f885ce53a5dcb4fe4bb18c68b4f11
Jan Amann <jan@amann.work>
2024-11-12 11:35:32 +0100

    read locale from `getLocale`, use hypothetical api to read params deeply, static messages import

--- examples/example-app-router/src/app/[locale]/layout.tsx
+++ examples/example-app-router/src/app/[locale]/layout.tsx
@@ -1,46 +1,29 @@
 import {notFound} from 'next/navigation';
-import {Locale, hasLocale} from 'next-intl';
-import {getTranslations} from 'next-intl/server';
-import {ReactNode, Suspense} from 'react';
+import {hasLocale} from 'next-intl';
+import {getLocale, getTranslations} from 'next-intl/server';
+import {ReactNode} from 'react';
 import BaseLayout from '@/components/BaseLayout';
 import {routing} from '@/i18n/routing';
 
 type Props = {
   children: ReactNode;
-  params: Promise<{locale: Locale}>;
 };
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({locale}));
 }
 
-export async function generateMetadata({params}: Omit<Props, 'children'>) {
-  const {locale} = await params;
-  const t = await getTranslations({locale, namespace: 'LocaleLayout'});
+export async function generateMetadata() {
+  const t = await getTranslations('LocaleLayout');
 
   return {
     title: t('title')
   };
 }
 
-export default function LocaleLayoutMain(props: Props) {
-  return (
-    <Suspense
-      fallback={
-        <html lang="en">
-          <body>
-            <div>Loading …</div>
-          </body>
-        </html>
-      }
-    >
-      <LocaleLayout {...props} />
-    </Suspense>
-  );
-}
-
-async function LocaleLayout({children, params}: Props) {
-  const {locale} = await params;
+export default async function LocaleLayout({children}: Props) {
+  // This is only necessary as long as there's no `dynamicParams = false`
+  const locale = await getLocale();
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
--- examples/example-app-router/src/i18n/request.ts
+++ examples/example-app-router/src/i18n/request.ts
@@ -1,16 +1,23 @@
 import {hasLocale} from 'next-intl';
 import {getRequestConfig} from 'next-intl/server';
+import messages from '../../messages/en.json';
+import {rootParams} from './future';
 import {routing} from './routing';
 
-export default getRequestConfig(async ({requestLocale}) => {
-  // Typically corresponds to the `[locale]` segment
-  const requested = await requestLocale;
-  const locale = hasLocale(routing.locales, requested)
-    ? requested
+async function now() {
+  'use cache';
+  return new Date();
+}
+
+export default getRequestConfig(async () => {
+  const params = await rootParams();
+  const locale = hasLocale(routing.locales, params.locale)
+    ? params.locale
     : routing.defaultLocale;
 
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages,
+    now: await now()
   };
 });
