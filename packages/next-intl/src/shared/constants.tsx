204b5dfe7464b7d83141d55e7455c824513fa574
Jan Amann <jan@amann.work>
2024-11-13 15:50:52 +0100

    feat!: Don't read a default for `locale` from `useParams.locale` on the client side

--- packages/next-intl/src/shared/constants.tsx
+++ packages/next-intl/src/shared/constants.tsx
@@ -1,5 +1,2 @@
 // Used to read the locale from the middleware
 export const HEADER_LOCALE_NAME = 'X-NEXT-INTL-LOCALE';
-
-// In a URL like "/en-US/about", the locale segment is "en-US"
-export const LOCALE_SEGMENT_NAME = 'locale';
