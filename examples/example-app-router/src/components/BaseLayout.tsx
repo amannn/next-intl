cf075fd21cdc86fa9d6c1dc74e1af645cf4c1657
Jan Amann <jan@amann.work>
2024-11-13 14:47:33 +0100

    move suspense up a level

--- examples/example-app-router/src/components/BaseLayout.tsx
+++ examples/example-app-router/src/components/BaseLayout.tsx
@@ -20,12 +20,12 @@ export default async function BaseLayout({children, locale}: Props) {
   return (
     <html className="h-full" lang={locale}>
       <body className={clsx(inter.className, 'flex h-full flex-col')}>
-        <NextIntlClientProvider messages={messages}>
-          <Suspense>
+        <Suspense>
+          <NextIntlClientProvider messages={messages}>
             <Navigation />
             {children}
-          </Suspense>
-        </NextIntlClientProvider>
+          </NextIntlClientProvider>
+        </Suspense>
       </body>
     </html>
   );
