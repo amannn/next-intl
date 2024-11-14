ef7192e80a03a84c004ce3595b66b5be0a6b3288
Jan Amann <jan@amann.work>
2024-11-14 17:11:20 +0100

    simplify

--- packages/use-intl/src/core/ICUArgs.tsx
+++ packages/use-intl/src/core/ICUArgs.tsx
@@ -1,22 +1,8 @@
-import {GetICUArgs} from '@schummar/icu-type-parser';
+import {GetICUArgs, GetICUArgsOptions} from '@schummar/icu-type-parser';
 
-type ICUArgs<
-  Message extends string,
-  ICUArgument,
-  ICUNumberArgument,
-  ICUDateArgument
-> =
+type ICUArgs<Message extends string, Options extends GetICUArgsOptions> =
   // This is important when `t` is returned from a function and there's no
   // known `Message` yet. Otherwise, we'd run into an infinite loop.
-  string extends Message
-    ? {}
-    : GetICUArgs<
-        Message,
-        {
-          ICUArgument: ICUArgument;
-          ICUNumberArgument: ICUNumberArgument;
-          ICUDateArgument: ICUDateArgument;
-        }
-      >;
+  string extends Message ? {} : GetICUArgs<Message, Options>;
 
 export default ICUArgs;
