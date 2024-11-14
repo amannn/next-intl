c8eb28350c0d9670cf6c8a3dbcb214668b500a52
Jan Amann <jan@amann.work>
2024-11-14 16:49:27 +0100

    safety check

--- packages/use-intl/src/core/ICUArgs.tsx
+++ packages/use-intl/src/core/ICUArgs.tsx
@@ -5,13 +5,18 @@ type ICUArgs<
   ICUArgument,
   ICUNumberArgument,
   ICUDateArgument
-> = GetICUArgs<
-  Message,
-  {
-    ICUArgument: ICUArgument;
-    ICUNumberArgument: ICUNumberArgument;
-    ICUDateArgument: ICUDateArgument;
-  }
->;
+> =
+  // This is important when `t` is returned from a function and there's no
+  // known `Message` yet. Otherwise, we'd run into an infinite loop.
+  string extends Message
+    ? {}
+    : GetICUArgs<
+        Message,
+        {
+          ICUArgument: ICUArgument;
+          ICUNumberArgument: ICUNumberArgument;
+          ICUDateArgument: ICUDateArgument;
+        }
+      >;
 
 export default ICUArgs;
