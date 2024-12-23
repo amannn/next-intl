a3037f8019bacea16d9164ee38a08fd52381467f
Jan Amann <jan@amann.work>
2024-12-23 09:24:38 +0100

    update docs and test

--- examples/example-app-router/tests/main.spec.ts
+++ examples/example-app-router/tests/main.spec.ts
@@ -95,7 +95,6 @@ it("sets a cookie when requesting a locale that doesn't match the `accept-langua
   expect(value).toContain('NEXT_LOCALE=de;');
   expect(value).toContain('Path=/;');
   expect(value).toContain('SameSite=lax');
-  expect(value).toContain('Expires=');
 });
 
 it('serves a robots.txt', async ({page}) => {
