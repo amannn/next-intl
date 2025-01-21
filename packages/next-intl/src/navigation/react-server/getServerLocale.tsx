39fa2cf032ed930161e7d179711d74c17c0c618a
Jan Amann <jan@amann.work>
2025-01-21 18:50:10 +0100

    `cache`

--- packages/next-intl/src/navigation/react-server/getServerLocale.tsx
+++ packages/next-intl/src/navigation/react-server/getServerLocale.tsx
@@ -1,8 +1,10 @@
+import {cache} from 'react';
+
 /**
  * This is only moved to a separate module for easier mocking in
  * `../createNavigation.test.tsx` in order to avoid suspending.
  */
-export default async function getServerLocale() {
+async function getServerLocaleImpl() {
   // Avoid bundling `next-intl/config` when `createNavigation` is used
   // https://github.com/amannn/next-intl/issues/1669
   const getConfig = (await import('../../server/react-server/getConfig'))
@@ -11,3 +13,6 @@ export default async function getServerLocale() {
   const config = await getConfig();
   return config.locale;
 }
+
+const getServerLocale = cache(getServerLocaleImpl);
+export default getServerLocale;
