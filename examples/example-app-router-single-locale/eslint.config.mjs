2bd1a7a4340d0731feeb8f860438f6769d71d396
Jan Amann <jan@amann.work>
2025-01-23 16:09:47 +0100

    enable nextjs eslint plugin and use standard config in examples

--- examples/example-app-router-single-locale/eslint.config.mjs
+++ examples/example-app-router-single-locale/eslint.config.mjs
@@ -1,3 +1,20 @@
-import {getPresets} from 'eslint-config-molindo';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import {FlatCompat} from '@eslint/eslintrc';
 
-export default await getPresets('typescript', 'react');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+});
+
+export default [
+  {
+    ignores: ['**/node_modules/*', '.next/*', 'dist/*']
+  },
+  ...compat.extends('next/core-web-vitals'),
+  {
+    rules: {
+      // Add any custom rules here
+    }
+  }
+];
--- examples/example-pages-router-legacy/eslint.config.mjs
+++ examples/example-pages-router-legacy/eslint.config.mjs
@@ -1,7 +1,21 @@
-import {getPresets} from 'eslint-config-molindo';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import {FlatCompat} from '@eslint/eslintrc';
+import js from '@eslint/js';
 
-export default (await getPresets('javascript', 'react')).concat({
-  rules: {
-    'react/prop-types': 'off'
-  }
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const compat = new FlatCompat({
+  baseDirectory: __dirname
 });
+
+const eslintConfig = [
+  js.configs.recommended,
+  ...compat.extends('next/core-web-vitals'),
+  {
+    rules: {
+      'react/prop-types': 'off'
+    }
+  }
+];
+
+export default eslintConfig;
