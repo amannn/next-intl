0e7aee694d733b32261862e8e865d22e92db5a2a
Jan Amann <jan@amann.work>
2025-02-17 16:56:36 +0100

    fix mixed routing example

--- examples/example-app-router-mixed-routing/src/middleware.ts
+++ examples/example-app-router-mixed-routing/src/middleware.ts
@@ -5,7 +5,7 @@ export default createMiddleware(routing);
 
 export const config = {
   // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … if they start with `/app`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
+  matcher: '/((?!app|_next|_vercel|.*\\..*).*)'
 };
