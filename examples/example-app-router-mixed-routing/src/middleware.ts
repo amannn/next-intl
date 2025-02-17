fd92c30615b3401318aac14100e0736f0ceee533
Jan Amann <jan@amann.work>
2025-02-17 16:46:48 +0100

    docs, examples

--- examples/example-app-router-mixed-routing/src/middleware.ts
+++ examples/example-app-router-mixed-routing/src/middleware.ts
@@ -4,6 +4,8 @@ import {routing} from './i18n/routing.public';
 export default createMiddleware(routing);
 
 export const config = {
-  // Match only public pathnames
-  matcher: ['/', '/(de|en)/:path*']
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
 };
--- examples/example-app-router/src/middleware.ts
+++ examples/example-app-router/src/middleware.ts
@@ -4,10 +4,8 @@ import {routing} from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: [
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
-  ]
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
 };
