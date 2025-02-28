d658b5cd31a6b0b3e195354cb215c41efeb27a53
Jan Amann <jan@amann.work>
2025-02-28 11:56:47 +0100

    fix conflict

--- examples/example-app-router-playground/src/app/[locale]/layout.tsx
+++ examples/example-app-router-playground/src/app/[locale]/layout.tsx
@@ -1,11 +1,5 @@
 import {Metadata} from 'next';
-<<<<<<< HEAD
 import {NextIntlClientProvider, useLocale} from 'next-intl';
-=======
-import {Inter} from 'next/font/google';
-import {notFound} from 'next/navigation';
-import {Locale, NextIntlClientProvider, hasLocale} from 'next-intl';
->>>>>>> origin/v4
 import {
   getFormatter,
   getNow,
@@ -13,10 +7,12 @@ import {
   getTranslations
 } from 'next-intl/server';
 import {ReactNode} from 'react';
+import {Inter} from 'next/font/google';
 import {routing} from '@/i18n/routing';
 import Navigation from '../../components/Navigation';
 
-<<<<<<< HEAD
+const inter = Inter({subsets: ['latin']});
+
 export async function generateStaticParams() {
   return routing.locales.map((locale) => ({locale}));
 }
@@ -28,25 +24,6 @@ export async function generateMetadata(): Promise<Metadata> {
   const formatter = await getFormatter();
   const now = await getNow();
   const timeZone = await getTimeZone();
-=======
-type Props = {
-  children: ReactNode;
-  params: Promise<{locale: Locale}>;
-};
-
-const inter = Inter({subsets: ['latin']});
-
-export async function generateMetadata(
-  props: Omit<Props, 'children'>
-): Promise<Metadata> {
-  const params = await props.params;
-  const {locale} = params;
-
-  const t = await getTranslations({locale, namespace: 'LocaleLayout'});
-  const formatter = await getFormatter({locale});
-  const now = await getNow({locale});
-  const timeZone = await getTimeZone({locale});
->>>>>>> origin/v4
 
   return {
     metadataBase: new URL('http://localhost:3000'),
@@ -59,22 +36,12 @@ export async function generateMetadata(
   };
 }
 
-<<<<<<< HEAD
 type Props = {
   children: ReactNode;
 };
 
 export default function LocaleLayout({children}: Props) {
   const locale = useLocale();
-=======
-export default async function LocaleLayout({params, children}: Props) {
-  const {locale} = await params;
-
-  // Ensure that the incoming `locale` is valid
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
->>>>>>> origin/v4
 
   return (
     <html className={inter.className} lang={locale}>
