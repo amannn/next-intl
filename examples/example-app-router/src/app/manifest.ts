7a4da910426084a7292072cfb3549e99ba8ddac1
Jan Amann <jan@amann.work>
2025-02-28 13:09:14 +0100

    remove some stuff so rootParams works

--- examples/example-app-router/src/app/manifest.ts
+++ examples/example-app-router/src/app/manifest.ts
@@ -5,7 +5,6 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
   const t = await getTranslations('Manifest');
 
   return {
-    // InvariantError: Invariant: Missing Client Reference Manifest for /manifest.webmanifest. This is a bug in Next.js.
     name: t('name'),
     start_url: '/',
     theme_color: '#101E33'
