1488bce0643756fa59592b08bc8bf6d4a18a997c
Jan Amann <jan@amann.work>
2025-03-17 18:43:31 +0100

    docs: Update Jest examples (#1797)
    Somehow the Jest examples in this repo still worked, but if you try to
    use `next-intl` outside of this monorepo there was an issue with Jest.
    The problem is that Jest doesn't support ESM (see e.g.
    https://github.com/vercel/next.js/issues/40183).
    
    Ref https://github.com/amannn/next-intl/issues/1796

--- docs/src/pages/docs/environments/_meta.tsx
+++ docs/src/pages/docs/environments/_meta.tsx
@@ -5,5 +5,6 @@ export default {
   'error-files': 'Error files (e.g. not-found)',
   mdx: 'Markdown (MDX)',
   'core-library': 'Core library',
-  'runtime-requirements': 'Runtime requirements'
+  'runtime-requirements': 'Runtime requirements',
+  testing: 'Testing'
 };
