0dd29299d67c0d5343ebb0facac35b06fbbe276a
Jan Amann <jan@amann.work>
2025-09-10 16:51:57 +0200

    fix: Avoid double-encoding of already encoded params

--- packages/next-intl/.size-limit.ts
+++ packages/next-intl/.size-limit.ts
@@ -15,7 +15,7 @@ const config: SizeLimitConfig = [
   {
     name: "import * from 'next-intl' (react-server)",
     path: 'dist/esm/production/index.react-server.js',
-    limit: '14.095 KB'
+    limit: '14.105 KB'
   },
   {
     name: "import {createNavigation} from 'next-intl/navigation' (react-client)",
