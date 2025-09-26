bbd84414deece232337625062cdcb8969f7cb0c9
Jan Amann <jan@amann.work>
2025-09-26 21:38:28 +0200

    wip

--- examples/example-app-router-extracted/transform-plugin/src/lib.rs
+++ examples/example-app-router-extracted/transform-plugin/src/lib.rs
@@ -100,21 +100,6 @@ impl VisitMut for TransformVisitor {
     }
 }
 
-/// An example plugin function with macro support.
-/// `plugin_transform` macro interop pointers into deserialized structs, as well
-/// as returning ptr back to host.
-///
-/// It is possible to opt out from macro by writing transform fn manually
-/// if plugin need to handle low-level ptr directly via
-/// `__transform_plugin_process_impl(
-///     ast_ptr: *const u8, ast_ptr_len: i32,
-///     unresolved_mark: u32, should_enable_comments_proxy: i32) ->
-///     i32 /*  0 for success, fail otherwise.
-///             Note this is only for internal pointer interop result,
-///             not actual transform result */`
-///
-/// This requires manual handling of serialization / deserialization from ptrs.
-/// Refer swc_plugin_macro to see how does it work internally.
 #[plugin_transform]
 pub fn process_transform(
     mut program: Program,
--- examples/example-app-router-extracted/transform-plugin/src/lib_test.rs
+++ examples/example-app-router-extracted/transform-plugin/src/lib_test.rs
@@ -1,13 +1,26 @@
 use crate::TransformVisitor;
 use swc_core::ecma::{transforms::testing::test_inline, visit::visit_mut_pass};
 
-// Test the message extraction and transformation
+// things to test:
+// - local rename of useExtracted
+// - const t =, but also let, var
+// - is order guaranteed?, for use_extracted_vars
+// - only t calls that are in scope of the original hook call
+
 test_inline!(
     Default::default(),
     |_| visit_mut_pass(TransformVisitor::new()),
     extract_use_extracted,
-    // Input codes
-    r#"import {useExtracted} from 'next-intl'; const t = useExtracted(); t("Hey from server!");"#,
-    // Output codes after transformed with plugin
-    r#"import {useTranslations} from 'next-intl'; const t = useTranslations(); t("mwFebS");"#
+    r#"
+import {useExtracted} from 'next-intl';
+
+const t = useExtracted();
+t("Hey from server!");
+    "#,
+    r#"
+import {useTranslations} from 'next-intl';
+
+const t = useTranslations();
+t("mwFebS");
+    "#
 );
