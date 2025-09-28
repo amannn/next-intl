4060a88099c6374d11fcb6e65b2473c2a61d22bb
Jan Amann <jan@amann.work>
2025-09-28 16:39:38 +0200

    wip

--- examples/example-app-router-extracted/src/app/page.tsx
+++ examples/example-app-router-extracted/src/app/page.tsx
@@ -3,10 +3,11 @@ import Client from './Client';
 
 export default function Index() {
   const t = useExtracted();
+  const name = 'Jane';
   return (
     <>
-      <h1>{t('Hey there!')}</h1>
-      <h1>{t('This is a test from the page.')}</h1>
+      <h1>{t('Hey {name}!', {name})}</h1>
+      <p>{t('This is a test from the page.')}</p>
       <Client />
     </>
   );
