ec3015e0feb03afe1761860d7870b8bd15088473
Jan Amann <jan@amann.work>
2025-09-30 16:47:09 +0200

    wip

--- packages/next-intl-extracted/crates/shared/src/traversal_test_transform.rs
+++ packages/next-intl-extracted/crates/shared/src/traversal_test_transform.rs
@@ -1,11 +1,9 @@
 use crate::traversal::{TraversalMode, UseExtractedVisitor};
-use swc_core::ecma::{transforms::testing::test_inline, visit::visit_mut_pass};
-
-// things to test:
-// - local rename of useExtracted
-// - const t =, but also let, var
-// - is order guaranteed?, for use_extracted_vars
-// - only t calls that are in scope of the original hook call
+use swc_core::ecma::{
+    parser::{EsSyntax, Syntax},
+    transforms::testing::test_inline,
+    visit::visit_mut_pass,
+};
 
 test_inline!(
     Default::default(),
@@ -25,6 +23,107 @@ import {useTranslations} from 'next-intl';
 function Component() {
     const t = useTranslations();
     t("mwFebS");
+}
+    "#
+);
+
+test_inline!(
+    Default::default(),
+    |_| visit_mut_pass(UseExtractedVisitor::new(TraversalMode::Transform)),
+    transform_use_extracted_with_let,
+    r#"
+import {useExtracted} from 'next-intl';
+
+function Component() {
+    let t = useExtracted();
+    t("Hey from server!");
+}
+    "#,
+    r#"
+import {useTranslations} from 'next-intl';
+
+function Component() {
+    let t = useTranslations();
+    t("mwFebS");
+}
+    "#
+);
+
+test_inline!(
+    Default::default(),
+    |_| visit_mut_pass(UseExtractedVisitor::new(TraversalMode::Transform)),
+    transform_use_extracted_t_out_of_scope,
+    r#"
+import {useExtracted} from 'next-intl';
+
+function Component() {
+    const t = useExtracted();
+    t("Hey from server!");
+}
+
+const t = (msg) => msg;
+t("Should not be transformed");
+    "#,
+    r#"
+import {useTranslations} from 'next-intl';
+
+function Component() {
+    const t = useTranslations();
+    t("mwFebS");
+}
+
+const t = (msg) => msg;
+t("Should not be transformed");
+    "#
+);
+
+test_inline!(
+    Syntax::Es(EsSyntax {
+        jsx: true,
+        ..Default::default()
+    }),
+    |_| visit_mut_pass(UseExtractedVisitor::new(TraversalMode::Transform)),
+    transform_use_extracted_complex,
+    r#"
+import {useState} from 'react';
+import {useExtracted} from 'next-intl';
+
+function Component() {
+    const [notification, setNotification] = useState();
+    const t = useExtracted();
+
+    function onClick() {
+        setNotification(t("Successfully sent!"));
+    }
+
+    return (
+        <div>
+            <button onClick={onClick}>
+                {t("Send")}
+            </button>
+            {notification}
+        </div>
+    );
+}
+    "#,
+    r#"
+import {useState} from 'react';
+import {useTranslations} from 'next-intl';
+
+function Component() {
+    const [notification, setNotification] = useState();
+    const t = useTranslations();
+
+    function onClick() {
+        setNotification(t("+1F2If"));
+    }
+
+    return <div>
+            <button onClick={onClick}>
+                {t("9WRlF4")}
+            </button>
+            {notification}
+        </div>;
 }
     "#
 );
