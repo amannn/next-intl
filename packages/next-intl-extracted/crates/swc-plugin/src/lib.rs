ac27ec15bf819f01c530be4b7638e67e81fad77b
Jan Amann <jan@amann.work>
2025-10-01 16:13:58 +0200

    wip

--- packages/next-intl-extracted/crates/swc-plugin/src/lib.rs
+++ packages/next-intl-extracted/crates/swc-plugin/src/lib.rs
@@ -1,3 +1,9 @@
+// problems with swc approach:
+// 1. we need a way to emit json file (not supported by swc)
+// 2. if we want to emit json file by js loader (in the same compile pass), we need to access swc output. for this we need this.loadModule, which is currently not supported
+// 3. so that means if we want to keep swc plugin, we need a sidecar watcher (which again comes with the problem of triple-specific rust builds)
+// 4. so maybe we should go for a js-based loader that does ast transform and also extraction. we can use global state there, diff as much as needed and only write the minimum
+
 use next_intl_shared::{TraversalMode, UseExtractedVisitor};
 use swc_core::{
     ecma::{
