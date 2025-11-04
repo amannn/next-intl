5ce10dceafd104f6cbaeb11f3f68fd2ac3012a6c
Jan Amann <jan@amann.work>
2025-11-04 22:19:25 +0100

    size

--- packages/next-intl/.size-limit.ts
+++ packages/next-intl/.size-limit.ts
@@ -4,7 +4,7 @@ const config: SizeLimitConfig = [
   {
     name: "import * from 'next-intl' (react-client)",
     path: 'dist/esm/production/index.react-client.js',
-    limit: '13.125 KB'
+    limit: '13.715 KB'
   },
   {
     name: "import {NextIntlClientProvider} from 'next-intl' (react-client)",
@@ -15,7 +15,7 @@ const config: SizeLimitConfig = [
   {
     name: "import * from 'next-intl' (react-server)",
     path: 'dist/esm/production/index.react-server.js',
-    limit: '14.105 KB'
+    limit: '14.215 KB'
   },
   {
     name: "import {createNavigation} from 'next-intl/navigation' (react-client)",
@@ -37,7 +37,7 @@ const config: SizeLimitConfig = [
   {
     name: "import * from 'next-intl/server' (react-server)",
     path: 'dist/esm/production/server.react-server.js',
-    limit: '13.395 KB'
+    limit: '13.485 KB'
   },
   {
     name: "import * from 'next-intl/middleware'",
