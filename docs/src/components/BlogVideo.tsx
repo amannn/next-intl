79f89dad016dcd76ca0c49da99e48fee8594e961
Jan Amann <jan@amann.work>
2025-11-07 15:47:47 +0100

    styling fix

--- docs/src/components/BlogVideo.tsx
+++ docs/src/components/BlogVideo.tsx
@@ -4,5 +4,5 @@ import Video from './Video';
 export default function BlogVideo(
   props: Omit<ComponentProps<typeof Video>, 'className'>
 ) {
-  return <Video className="-mx-3 my-4 xl:-mx-6 xl:my-8" {...props} />;
+  return <Video className="-mx-3 my-5 xl:-mx-6 xl:my-8" {...props} />;
 }
