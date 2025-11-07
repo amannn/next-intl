7e95eb2fb8e588c028aa58773892e57531a1224c
Jan Amann <jan@amann.work>
2025-11-07 15:43:27 +0100

    docs finishing touches

--- docs/src/pages/docs/usage/_meta.tsx
+++ docs/src/pages/docs/usage/_meta.tsx
@@ -4,6 +4,6 @@ export default {
   'dates-times': 'Dates and times',
   lists: 'Lists',
   configuration: 'Request configuration',
-  extraction: 'Message extraction',
+  extraction: 'useExtracted',
   plugin: 'Next.js plugin'
 };
