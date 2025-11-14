29b634c7f1edb61a1b9f202f284f4573a62d39e1
DongYun Kang <kdy.1997.dev@gmail.com>
2025-11-14 18:48:04 +0900

    rename_all

--- packages/swc-plugin/src/lib.rs
+++ packages/swc-plugin/src/lib.rs
@@ -6,8 +6,10 @@ use rustc_hash::FxHashMap;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha512};
 use swc_atoms::Wtf8Atom;
-use swc_common::{DUMMY_SP, Spanned, errors::HANDLER};
-use swc_core::{plugin::proxies::TransformPluginProgramMetadata, transform_common::output::experimental_emit};
+use swc_common::{errors::HANDLER, Spanned, DUMMY_SP};
+use swc_core::{
+    plugin::proxies::TransformPluginProgramMetadata, transform_common::output::experimental_emit,
+};
 use swc_ecma_ast::*;
 use swc_ecma_utils::{private_ident, ExprFactory};
 use swc_ecma_visit::{VisitMut, VisitMutWith};
@@ -22,13 +24,13 @@ fn next_intl_plugin(mut program: Program, data: TransformPluginProgramMetadata) 
     )
     .expect("invalid config for next-intl");
 
-    let mut visitor = TransformVisitor::new(
-        config.is_development,
-        config.file_path,
-    );  
+    let mut visitor = TransformVisitor::new(config.is_development, config.file_path);
     program.visit_mut_with(&mut visitor);
 
-    experimental_emit("results".into(), serde_json::to_string(&visitor.results).unwrap());
+    experimental_emit(
+        "results".into(),
+        serde_json::to_string(&visitor.results).unwrap(),
+    );
 
     program
 }
@@ -36,6 +38,7 @@ fn next_intl_plugin(mut program: Program, data: TransformPluginProgramMetadata) 
 const NAMESPACE_SEPARATOR: &str = ".";
 
 #[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct Config {
     is_development: bool,
     file_path: String,
