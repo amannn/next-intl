b2a7448e075d4095bdc54fa9007d91058574b30e
Jan Amann <jan@amann.work>
2025-12-05 13:00:03 +0100

    clean up types

--- packages/next-intl/src/extractor/index.tsx
+++ packages/next-intl/src/extractor/index.tsx
@@ -1,5 +1,2 @@
 export {default as unstable_extractMessages} from './extractMessages.js';
-
-export type {default as ExtractorCodec} from './format/ExtractorCodec.js';
-export type {ExtractorCodecContext} from './format/ExtractorCodec.js';
-export type {ExtractorMessage} from './types.js';
+export {defineCodec} from './format/ExtractorCodec.js';
