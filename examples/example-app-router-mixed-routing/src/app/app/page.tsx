3610e5ae474038849424a8d8ef905d31de859b4e
Jan Amann <jan@amann.work>
2025-12-25 14:33:05 +0100

    Remove example-app-router-mixed-routing

--- examples/example-app-router-mixed-routing/src/app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import {useTranslations} from 'next-intl';
-import PageTitle from '@/components/PageTitle';
-
-export default function App() {
-  const t = useTranslations('App');
-  return <PageTitle>{t('title')}</PageTitle>;
-}
