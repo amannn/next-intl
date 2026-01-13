dffaca8bc61bf8b93bcd112072d0ba6e84952335
Jan Amann <jan@amann.work>
2026-01-13 09:37:24 +0100

    wip

--- packages/icu-minify/.size-limit.ts
+++ packages/icu-minify/.size-limit.ts
@@ -5,13 +5,13 @@ const config: SizeLimitConfig = [
     name: "import format from 'icu-minify/format' (production)",
     import: 'format',
     path: 'dist/esm/production/format.js',
-    limit: '0.785 kB'
+    limit: '0.775 kB'
   },
   {
     name: "import compile from 'icu-minify/compiler' (production)",
     import: 'compile',
     path: 'dist/esm/production/compiler.js',
-    limit: '8.42 kB'
+    limit: '8.425 kB'
   }
 ];
 
