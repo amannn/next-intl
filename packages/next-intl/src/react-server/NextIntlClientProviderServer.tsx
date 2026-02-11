dff6e49470aaa93b130eeed314e6aa1b141864c9
Jan Amann <jan@amann.work>
2026-02-11 16:35:19 +0100

    new debugger

--- packages/next-intl/src/react-server/NextIntlClientProviderServer.tsx
+++ packages/next-intl/src/react-server/NextIntlClientProviderServer.tsx
@@ -12,17 +12,8 @@ type Props = ComponentProps<typeof BaseNextIntlClientProvider>;
 type ResolvedMessages = Exclude<Props['messages'], 'infer'>;
 
 async function resolveMessages(
-  messages: Props['messages'],
   tempSegment: string | undefined
 ): Promise<ResolvedMessages> {
-  if (messages === undefined) {
-    return await getMessages();
-  }
-
-  if (messages !== 'infer') {
-    return messages;
-  }
-
   const allMessages = await getMessages();
   const manifest = await loadTreeShakingManifest();
   if (!manifest) {
@@ -46,13 +37,22 @@ export default async function NextIntlClientProviderServer({
   timeZone,
   ...rest
 }: Props) {
-  return (
+  let clientMessages;
+  if (messages === undefined) {
+    clientMessages = await getMessages();
+  } else if (messages === 'infer') {
+    clientMessages = await resolveMessages(temp_segment);
+  } else {
+    clientMessages = messages;
+  }
+
+  const result = (
     <BaseNextIntlClientProvider
       // We need to be careful about potentially reading from headers here.
       // See https://github.com/amannn/next-intl/issues/631
       formats={formats === undefined ? await getFormats() : formats}
       locale={locale ?? (await getLocale())}
-      messages={await resolveMessages(messages, temp_segment)}
+      messages={clientMessages}
       // Note that we don't assign a default for `now` here,
       // we only read one from the request config - if any.
       // Otherwise this would cause a `dynamicIO` error.
@@ -61,4 +61,18 @@ export default async function NextIntlClientProviderServer({
       {...rest}
     />
   );
+
+  // Temporary debugging
+  if (process.env.NODE_ENV !== 'production' && messages === 'infer') {
+    return (
+      <div style={{border: '1px solid green'}}>
+        <pre style={{backgroundColor: 'lightgreen'}}>
+          {JSON.stringify(clientMessages, null, 2)}
+        </pre>
+        {result}
+      </div>
+    );
+  }
+
+  return result;
 }
