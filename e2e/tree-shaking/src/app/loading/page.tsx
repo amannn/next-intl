51e6cde3336df0778ef2fcd9c35e84e61be7b9b1
Jan Amann <jan@amann.work>
2026-02-20 11:57:02 +0100

    simplify

--- e2e/tree-shaking/src/app/loading/page.tsx
+++ e2e/tree-shaking/src/app/loading/page.tsx
@@ -1,16 +1,9 @@
-import DebugMessages from '@/components/DebugMessages';
 import {getExtracted} from 'next-intl/server';
-import {NextIntlClientProvider} from 'next-intl';
 
 export const dynamic = 'force-dynamic';
 
 export default async function LoadingPage() {
   const t = await getExtracted();
   await new Promise((resolve) => setTimeout(resolve, 1500));
-  return (
-    <NextIntlClientProvider messages="infer">
-      <DebugMessages />
-      <p>{t('Static page')}</p>
-    </NextIntlClientProvider>
-  );
+  return <p>{t('Static page')}</p>;
 }
