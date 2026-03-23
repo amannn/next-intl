7e34934237fc80151880b0e35df847409a4efca0
Wojciech Maj <kontakt@wojtekmaj.pl>
2026-03-23 23:14:35 +0100

    Update TypeScript to v6
    This is an EXPERIMENTAL PR that aims to update TypeScript to v6.
    
    Considerations:
    - typescript-eslint has a peerDependency on TypeScript v5.
    - es5 target is deprecated (at last!) so this also updates target to es2015 (still ancient, but may be a breaking change?)

---
+++ docs/global.d.ts
@@ -0,0 +1,9 @@
+declare module '*.module.css' {
+  const classes: Record<string, string>;
+  export default classes;
+}
+
+declare module '@docsearch/css';
+declare module 'nextra-theme-docs/style.css';
+
+declare module '*.css';
