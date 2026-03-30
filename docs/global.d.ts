0198eee684c513438797f5c13ceb964e33465aa6
Wojciech Maj <kontakt@wojtekmaj.pl>
2026-03-30 12:20:48 +0200

    Minimize changes

--- docs/global.d.ts
+++ docs/global.d.ts
@@ -2,8 +2,3 @@ declare module '*.module.css' {
   const classes: Record<string, string>;
   export default classes;
 }
-
-declare module '@docsearch/css';
-declare module 'nextra-theme-docs/style.css';
-
-declare module '*.css';
