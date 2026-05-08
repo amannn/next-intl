91d9da5b03c1970c19f6ef71fdf0610745639d79
Adebesin Tolulope <adebesintolulope80@gmail.com>
2026-05-08 12:45:31 +0100

    feat(playground): add link annotation handler

---
+++ playground/src/components/code/annotations/link.tsx
@@ -0,0 +1,15 @@
+import type { AnnotationHandler } from 'codehike/code';
+
+export const link: AnnotationHandler = {
+  name: 'link',
+  Inline: ({ annotation, children }) => (
+    <a
+      href={annotation.query}
+      className="underline decoration-dotted underline-offset-4 hover:text-primary"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {children}
+    </a>
+  ),
+};
