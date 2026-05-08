4aac59164c0c96241471cabedb4f3abe958d44b0
Adebesin Tolulope <adebesintolulope80@gmail.com>
2026-05-08 12:53:49 +0100

    feat(playground): export annotation handlers from barrel

---
+++ playground/src/components/code/annotations/index.ts
@@ -0,0 +1,6 @@
+export { mark } from './mark';
+export { callout } from './callout';
+export { focus } from './focus';
+export { link } from './link';
+export { fold } from './fold';
+export { lineNumbers } from './line-numbers';
