d2da339c6745ca7247a1a80f99fed2256c29a433
Adebesin Tolulope <adebesintolulope80@gmail.com>
2026-05-08 12:32:53 +0100

    feat(playground): add focus annotation handler

---
+++ playground/src/components/code/annotations/focus.tsx
@@ -0,0 +1,18 @@
+import type { AnnotationHandler } from 'codehike/code';
+import { InnerLine } from 'codehike/code';
+import { PreWithFocus } from './focus.client';
+
+export const focus: AnnotationHandler = {
+  name: 'focus',
+  onlyIfAnnotated: true,
+  PreWithRef: PreWithFocus,
+  Line: (props) => (
+    <InnerLine
+      merge={props}
+      className="opacity-50 data-[focus]:opacity-100 px-2"
+    />
+  ),
+  AnnotatedLine: ({ annotation, ...props }) => (
+    <InnerLine merge={props} data-focus={true} className="bg-zinc-700/30" />
+  ),
+};
