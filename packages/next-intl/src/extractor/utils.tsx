3754cb31b2514ec920922198aa341a5703b807cf
Jan Amann <jan@amann.work>
2026-05-11 11:38:48 +0200

    add warning for missing references

--- packages/next-intl/src/extractor/utils.tsx
+++ packages/next-intl/src/extractor/utils.tsx
@@ -1,4 +1,5 @@
 import path from 'path';
+import {warn} from '../plugin/utils.js';
 import type {ExtractorMessage, ExtractorMessageReference} from './types.js';
 
 export function normalizePathToPosix(filePath: string): string {
@@ -51,13 +52,33 @@ export function setNestedProperty(
 export function getSortedMessages(
   messages: Array<ExtractorMessage>
 ): Array<ExtractorMessage> {
+  const warnedMissingReferenceIds = new Set<string>();
+
   return messages.toSorted((messageA, messageB) => {
     const refA = messageA.references[0];
     const refB = messageB.references[0];
 
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (refA == null) {
+      warnAboutMissingReference(messageA.id, warnedMissingReferenceIds);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (refB == null) {
+      warnAboutMissingReference(messageB.id, warnedMissingReferenceIds);
+    }
+
     // Sort by path, then line. Same path+line: preserve original order
     return compareReferences(refA, refB);
   });
+}
+
+function warnAboutMissingReference(
+  id: string,
+  warnedMissingReferenceIds: Set<string>
+): void {
+  if (warnedMissingReferenceIds.has(id)) return;
+  warnedMissingReferenceIds.add(id);
+  warn(`Missing file reference for extracted message: ${id}`);
 }
 
 export function localeCompare(a: string, b: string) {
