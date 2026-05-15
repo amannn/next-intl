3bdd696dd6f15fce632cc56514b0ec2895d851ab
Jan Amann <jan@amann.work>
2026-05-15 12:14:39 +0200

    wip

--- examples/example-app-router-playground/src/app/[locale]/actions/ListItem.tsx
+++
@@ -1,6 +0,0 @@
-import {useTranslations} from 'next-intl';
-
-export default function ListItem({id}: {id: number}) {
-  const t = useTranslations('ServerActions');
-  return t('item', {id: String(id)});
-}
--- examples/example-app-router-playground/src/app/[locale]/opengraph-image.tsx
+++ examples/example-app-router-playground/src/app/[locale]/opengraph-image.tsx
@@ -1,7 +1,16 @@
 import {ImageResponse} from 'next/og';
+import {Locale} from 'next-intl';
 import {getTranslations} from 'next-intl/server';
 
-export default async function Image() {
-  const t = await getTranslations('OpenGraph');
+export default async function Image({
+  params
+}: {
+  params: Promise<{locale: string}>;
+}) {
+  const {locale} = await params;
+  const t = await getTranslations({
+    namespace: 'OpenGraph',
+    locale: locale as Locale
+  });
   return new ImageResponse(<div style={{fontSize: 128}}>{t('title')}</div>);
 }
