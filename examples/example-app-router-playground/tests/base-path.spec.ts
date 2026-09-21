0e2655368e165fe3d018a0ebed4f503018a66466
Yaqoubj <yaqoffi12@gmail.com>
2026-09-21 12:04:44 +0300

    test: await locale cookie checks in base-path navigation (#2418)

--- examples/example-app-router-playground/tests/base-path.spec.ts
+++ examples/example-app-router-playground/tests/base-path.spec.ts
@@ -11,14 +11,14 @@ it('updates the cookie correctly', async ({page}) => {
   await page.getByRole('link', {name: 'Switch to German'}).click();
 
   await expect(page).toHaveURL('/base/path/de');
-  assertLocaleCookieValue(page, 'de', {path: '/base/path'});
+  await assertLocaleCookieValue(page, 'de', {path: '/base/path'});
   await page.getByRole('button', {name: 'Go to nested page'}).click();
   await expect(page).toHaveURL('/base/path/de/verschachtelt');
   await page.getByRole('link', {name: 'Start'}).click();
   await page.getByRole('link', {name: 'Zu Englisch wechseln'}).click();
 
   await expect(page).toHaveURL('/base/path');
-  assertLocaleCookieValue(page, 'en', {path: '/base/path'});
+  await assertLocaleCookieValue(page, 'en', {path: '/base/path'});
 });
 
 it('updates the cookie correctly when using the router', async ({page}) => {
